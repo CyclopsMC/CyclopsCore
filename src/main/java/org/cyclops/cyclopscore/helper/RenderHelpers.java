ac58917a61abcd3cfc1e2f2869b2ddf2f0bc10e2
Ruben Taelman <rubensworks@gmail.com>
2015-06-06 12:01:11 +0200

    Add support for scaled string rendering in guis

--- src/main/java/org/cyclops/cyclopscore/helper/RenderHelpers.java
+++ src/main/java/org/cyclops/cyclopscore/helper/RenderHelpers.java
@@ -1,6 +1,8 @@
 package org.cyclops.cyclopscore.helper;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
 
 /**
@@ -28,5 +30,55 @@ public class RenderHelpers {
 	public static int RGBToInt(int r, int g, int b) {
 	    return (int)r << 16 | (int)g << 8 | (int)b;
 	}
+
+    /**
+     * Draw the given text and scale it to the max width.
+     * @param fontRenderer The font renderer
+     * @param string The string to draw
+     * @param x The center X
+     * @param y The center Y
+     * @param maxWidth The maximum width to scale to
+     * @param color The color to draw
+     */
+    public static void drawScaledCenteredString(FontRenderer fontRenderer, String string, int x, int y, int maxWidth, int color) {
+        drawScaledCenteredString(fontRenderer, string, x, y, maxWidth, 1.0F, maxWidth, color);
+    }
+
+    /**
+     * Draw the given text and scale it to the max width.
+     * The given string may already be scaled and its width must be passed in that case.
+     * @param fontRenderer The font renderer
+     * @param string The string to draw
+     * @param x The center X
+     * @param y The center Y
+     * @param width The scaled width
+     * @param originalScale The original scale
+     * @param maxWidth The maximum width to scale to
+     * @param color The color to draw
+     */
+    public static void drawScaledCenteredString(FontRenderer fontRenderer, String string, int x, int y, int width, float originalScale, int maxWidth, int color) {
+        float originalWidth = fontRenderer.getStringWidth(string) * originalScale;
+        float scale = Math.min(originalScale, maxWidth / originalWidth * originalScale);
+        drawScaledCenteredString(fontRenderer, string, x, y, width, scale, color);
+    }
+
+    /**
+     * Draw the given text with the given width and desired scale.
+     * @param fontRenderer The font renderer
+     * @param string The string to draw
+     * @param x The center X
+     * @param y The center Y
+     * @param width The scaled width
+     * @param scale The desired scale
+     * @param color The color to draw
+     */
+    public static void drawScaledCenteredString(FontRenderer fontRenderer, String string, int x, int y, int width, float scale, int color) {
+        GlStateManager.pushMatrix();
+        GlStateManager.scale(scale, scale, 1.0f);
+        int titleLength = fontRenderer.getStringWidth(string);
+        int titleHeight = fontRenderer.FONT_HEIGHT;
+        fontRenderer.drawString(string, Math.round((x + width / 2) / scale - titleLength / 2), Math.round(y / scale - titleHeight / 2), color);
+        GlStateManager.popMatrix();
+    }
 
 }
