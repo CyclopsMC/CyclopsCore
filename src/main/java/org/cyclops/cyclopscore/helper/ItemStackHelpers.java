c24c91b723129df7186cdd4eedf482c9f086ee53
Ruben Taelman <rubensworks@gmail.com>
2015-06-26 14:55:20 +0200

    Add helpers for spawning items in world.

--- src/main/java/org/cyclops/cyclopscore/helper/ItemStackHelpers.java
+++ src/main/java/org/cyclops/cyclopscore/helper/ItemStackHelpers.java
@@ -1,13 +1,20 @@
 package org.cyclops.cyclopscore.helper;
 
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 /**
  * Contains helper methods for various itemstack specific things.
  * @author rubensworks
  */
 public final class ItemStackHelpers {
+
+    private static final Random RANDOM = new Random();
 
     /**
      * Get the tag compound from an item safely.
@@ -20,6 +27,52 @@ public final class ItemStackHelpers {
             itemStack.setTagCompound(new NBTTagCompound());
         }
         return itemStack.getTagCompound();
+    }
+
+    /**
+     * Spawn an itemstack into the world.
+     * @param world The world
+     * @param pos The position
+     * @param itemStack the item stack
+     */
+    public static void spawnItemStack(World world, BlockPos pos, ItemStack itemStack) {
+        spawnItemStack(world, pos.getX(), pos.getY(), pos.getZ(), itemStack);
+    }
+
+    /**
+     * Spawn an itemstack into the world.
+     * @param world The world
+     * @param x X
+     * @param y Y
+     * @param z Z
+     * @param itemStack the item stack
+     */
+    public static void spawnItemStack(World world, double x, double y, double z, ItemStack itemStack) {
+        float offsetX = RANDOM.nextFloat() * 0.8F + 0.1F;
+        float offsetY = RANDOM.nextFloat() * 0.8F + 0.1F;
+        float offsetZ = RANDOM.nextFloat() * 0.8F + 0.1F;
+
+        while (itemStack.stackSize > 0) {
+            int i = RANDOM.nextInt(21) + 10;
+
+            if (i > itemStack.stackSize) {
+                i = itemStack.stackSize;
+            }
+
+            itemStack.stackSize -= i;
+            EntityItem entityitem = new EntityItem(world, x + (double)offsetX, y + (double)offsetY,
+                    z + (double)offsetZ, new ItemStack(itemStack.getItem(), i, itemStack.getMetadata()));
+
+            if (itemStack.hasTagCompound()) {
+                entityitem.getEntityItem().setTagCompound((NBTTagCompound)itemStack.getTagCompound().copy());
+            }
+
+            float motion = 0.05F;
+            entityitem.motionX = RANDOM.nextGaussian() * (double)motion;
+            entityitem.motionY = RANDOM.nextGaussian() * (double)motion + 0.2D;
+            entityitem.motionZ = RANDOM.nextGaussian() * (double)motion;
+            world.spawnEntityInWorld(entityitem);
+        }
     }
 
 }
