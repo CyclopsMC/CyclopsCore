78084851a76dc6f3889a04cfbf8ab2fb3dd98065
Ruben Taelman <rubensworks@gmail.com>
2015-07-02 11:11:55 +0200

    Fix rendering of models sometimes becoming invisible

--- src/main/java/org/cyclops/cyclopscore/client/render/tileentity/RenderTileEntityBakedModel.java
+++ src/main/java/org/cyclops/cyclopscore/client/render/tileentity/RenderTileEntityBakedModel.java
@@ -5,6 +5,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockModelShapes;
 import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.resources.model.IBakedModel;
 import org.cyclops.cyclopscore.tileentity.CyclopsTileEntity;
 import org.lwjgl.opengl.GL11;
@@ -25,7 +26,12 @@ public abstract class RenderTileEntityBakedModel<T extends CyclopsTileEntity> ex
     }
 
     protected void renderTileEntityAt(T tile, double x, double y, double z, float partialTick, int destroyStage) {
+        GlStateManager.enableRescaleNormal();
+        GlStateManager.alphaFunc(516, 0.1F);
+        GlStateManager.enableBlend();
+        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
         GlStateManager.pushMatrix();
+        GlStateManager.pushAttrib();
 
         GL11.glEnable(GL12.GL_RESCALE_NORMAL);
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
@@ -36,16 +42,20 @@ public abstract class RenderTileEntityBakedModel<T extends CyclopsTileEntity> ex
         BlockRendererDispatcher blockRendererDispatcher = mc.getBlockRendererDispatcher();
         BlockModelShapes blockModelShapes = blockRendererDispatcher.getBlockModelShapes();
         IBakedModel bakedModel = blockModelShapes.getModelForState(getBlockState(tile, x, y, z, partialTick, destroyStage));
+        Minecraft.getMinecraft().getTextureManager().bindTexture(TextureMap.locationBlocksTexture);
         renderModel(tile, bakedModel, partialTick);
 
+        GlStateManager.popAttrib();
         GlStateManager.popMatrix();
+        GlStateManager.disableRescaleNormal();
+        GlStateManager.disableBlend();
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
     }
 
     protected abstract IBlockState getBlockState(T tile, double x, double y, double z, float partialTick, int destroyStage);
 
     @Override
-	protected void renderModel(T tile, IBakedModel model, float partialTick) {
+    protected void renderModel(T tile, IBakedModel model, float partialTick) {
         Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelRenderer().
                 renderModelBrightnessColor(model, 1.0F, 1.0F, 1.0F, 1.0F);
     }
