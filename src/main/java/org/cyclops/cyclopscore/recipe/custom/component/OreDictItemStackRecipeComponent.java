ff74a8acb10aab9241b6c356956f18ddbf0bb162
Ruben Taelman <rubensworks@gmail.com>
2015-07-08 13:39:19 +0200

    Port fluid related abstraction from EvilCraft

--- src/main/java/org/cyclops/cyclopscore/recipe/custom/component/OreDictItemStackRecipeComponent.java
+++ src/main/java/org/cyclops/cyclopscore/recipe/custom/component/OreDictItemStackRecipeComponent.java
@@ -1,7 +1,6 @@
 package org.cyclops.cyclopscore.recipe.custom.component;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
@@ -14,7 +13,6 @@ import java.util.List;
  *
  * @author immortaleeb
  */
-@EqualsAndHashCode(callSuper = false)
 @Data
 public class OreDictItemStackRecipeComponent extends ItemStackRecipeComponent {
 
