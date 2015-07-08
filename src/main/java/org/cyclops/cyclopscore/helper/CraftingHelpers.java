0f335973b6011cc770d18f7fb06d50c1532d2463
Ruben Taelman <rubensworks@gmail.com>
2015-07-08 12:58:30 +0200

    Port all relevant helpers from EvilCraft

---
+++ src/main/java/org/cyclops/cyclopscore/helper/CraftingHelpers.java
@@ -0,0 +1,50 @@
+package org.cyclops.cyclopscore.helper;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.FurnaceRecipes;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.oredict.OreDictionary;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Several convenience functions for crafting.
+ * @author rubensworks
+ */
+public class CraftingHelpers {
+
+    public static IRecipe findCraftingRecipe(ItemStack itemStack, int index) throws IllegalArgumentException {
+        int indexAttempt = index;
+        for(IRecipe recipe : (List<IRecipe>) CraftingManager.getInstance().getRecipeList()) {
+            if(itemStacksEqual(recipe.getRecipeOutput(), itemStack) && indexAttempt-- == 0) {
+                return recipe;
+            }
+        }
+        throw new IllegalArgumentException("Could not find crafting recipe for " + itemStack.getItem().getUnlocalizedName() +
+                "with index " + index);
+    }
+
+    public static Map.Entry<ItemStack, ItemStack> findFurnaceRecipe(ItemStack itemStack, int index) throws IllegalArgumentException {
+        int indexAttempt = index;
+        for(Map.Entry<ItemStack, ItemStack> recipe : ((Map<ItemStack, ItemStack>) FurnaceRecipes.instance().
+                getSmeltingList()).entrySet()) {
+            if(itemStacksEqual(recipe.getValue(), itemStack) && indexAttempt-- == 0) {
+                return recipe;
+            }
+        }
+        throw new IllegalArgumentException("Could not find furnace recipe for " + itemStack.getItem().getUnlocalizedName() +
+                "with index " + index);
+    }
+
+    public static boolean itemStacksEqual(ItemStack itemStack1, ItemStack itemStack2) {
+        return itemStack1 != null && itemStack2 != null &&
+               itemStack1.getItem() == itemStack2.getItem() &&
+               (itemStack1.getItemDamage() == itemStack2.getItemDamage() ||
+                       itemStack1.getItemDamage() == OreDictionary.WILDCARD_VALUE ||
+                       itemStack2.getItemDamage() == OreDictionary.WILDCARD_VALUE ||
+                       itemStack1.getItem().isDamageable());
+    }
+
+}
--- src/main/java/org/cyclops/cyclopscore/helper/WorldHelpers.java
+++ src/main/java/org/cyclops/cyclopscore/helper/WorldHelpers.java
@@ -68,6 +68,18 @@ public class WorldHelpers {
 		for(int param : params) offset += param;
 		return world.rand.nextInt(mod) == Math.abs(offset) % mod;
 	}
+
+    /**
+     * Check if an efficient tick can happen.
+     * This is useful for opererations that should happen frequently, but not strictly every tick.
+     * @param world The world to tick in.
+     * @param baseModulus The amount of ticks that could be skipped.
+     * @param blockPos The position to use as param.
+     * @return If a tick of some operation can occur.
+     */
+    public static boolean efficientTick(World world, int baseModulus, BlockPos blockPos) {
+        return efficientTick(world, baseModulus, blockPos.getX(), blockPos.getY(), blockPos.getZ());
+    }
 	
 	/**
 	 * Get the chunk location (coordinates within one chunk.) from a world location.
@@ -83,26 +95,40 @@ public class WorldHelpers {
     /**
      * Loop over a 3D area while accumulating a value.
      * @param world The world.
-     * @param area Radius.
-     * @param pos The center position.
+     * @param areaMin Radius array to start from {x, y, z}.
+     * @param areaMax Radius array to end at (inclusive) {x, y, z}.
+     * @param blockPos The position.
      * @param folder The folding function.
      * @param value The start value.
      * @param <T> The type of value to accumulate.
      * @return The resulting value.
      */
-    public static <T> T foldArea(World world, int area, BlockPos pos, WorldFoldingFunction<T, T> folder, T value) {
-        int x, y, z;
-        x = pos.getX();
-        y = pos.getY();
-        z = pos.getZ();
-        for(int xc = x - area; xc <= x + area; xc++) {
-            for(int yc = y - area; yc <= y + area; yc++) {
-                for(int zc = z - area; zc <= z + area; zc++) {
+    public static <T> T foldArea(World world, int[] areaMin, int[] areaMax, BlockPos blockPos, WorldFoldingFunction<T, T> folder, T value) {
+        int x = blockPos.getX();
+        int y = blockPos.getY();
+        int z = blockPos.getZ();
+        for(int xc = x - areaMin[0]; xc <= x + areaMax[0]; xc++) {
+            for(int yc = y - areaMin[1]; yc <= y + areaMax[1]; yc++) {
+                for(int zc = z - areaMin[2]; zc <= z + areaMax[2]; zc++) {
                     value = folder.apply(value, world, new BlockPos(xc, yc, zc));
                 }
             }
         }
         return value;
+    }
+
+    /**
+     * Loop over a 3D area while accumulating a value.
+     * @param world The world.
+     * @param area Radius.
+     * @param blockPos The position.
+     * @param folder The folding function.
+     * @param value The start value.
+     * @param <T> The type of value to accumulate.
+     * @return The resulting value.
+     */
+    public static <T> T foldArea(World world, int area, BlockPos blockPos, WorldFoldingFunction<T, T> folder, T value) {
+        return foldArea(world, new int[]{area, area, area}, new int[]{area, area, area}, blockPos, folder, value);
     }
 
     public static interface WorldFoldingFunction<F, T> {
