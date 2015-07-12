f0b49190b6f244302098aca3d0429e743d7541ee
immortaleeb <immortaleeb@gmail.com>
2015-07-12 22:42:51 +0200

    Add config for BlockFluids
    BlockFluids need additional registration on the client side for
    rendering fluids, which is now handled by BlockFluidConfig.

---
+++ src/main/java/org/cyclops/cyclopscore/config/extendedconfig/BlockFluidConfig.java
@@ -0,0 +1,70 @@
+package org.cyclops.cyclopscore.config.extendedconfig;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.ItemMeshDefinition;
+import net.minecraft.client.renderer.block.statemap.StateMapperBase;
+import net.minecraft.client.resources.model.ModelBakery;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fluids.BlockFluidClassic;
+import org.cyclops.cyclopscore.helper.MinecraftHelpers;
+import org.cyclops.cyclopscore.init.ModBase;
+
+/**
+ * Config for BlockFluids.
+ * @author immortaleeb
+ * @see ExtendedConfig
+ */
+public abstract class BlockFluidConfig extends BlockConfig {
+
+    private ModelResourceLocation fluidLocation;
+
+    /**
+     * Make a new instance.
+     *
+     * @param mod     The mod instance.
+     * @param enabled If this should is enabled.
+     * @param namedId The unique name ID for the configurable.
+     * @param comment The comment to add in the config file for this configurable.
+     * @param element The class of this configurable.
+     */
+    public BlockFluidConfig(ModBase mod, boolean enabled, String namedId, String comment, Class<? extends BlockFluidClassic> element) {
+        super(mod, enabled, namedId, comment, element);
+
+        fluidLocation = new ModelResourceLocation(mod.getModId() + ":" + getNamedId(), "fluid");
+    }
+
+    @Override
+    public BlockFluidClassic getBlockInstance() {
+        return (BlockFluidClassic) super.getBlockInstance();
+    }
+
+    @Override
+    public void onRegistered() {
+        super.onRegistered();
+
+        // Handle registration for fluid rendering
+        if (MinecraftHelpers.isClientSide()) {
+            BlockFluidClassic blockInstance = getBlockInstance();
+            Item fluid = Item.getItemFromBlock(blockInstance);
+
+            ModelBakery.addVariantName(fluid);
+            ModelLoader.setCustomMeshDefinition(fluid, new ItemMeshDefinition() {
+                @Override
+                public ModelResourceLocation getModelLocation(ItemStack stack) {
+                    return fluidLocation;
+                }
+            });
+
+            ModelLoader.setCustomStateMapper(blockInstance, new StateMapperBase() {
+
+                @Override
+                protected ModelResourceLocation getModelResourceLocation(IBlockState state) {
+                    return fluidLocation;
+                }
+            });
+        }
+    }
+}
