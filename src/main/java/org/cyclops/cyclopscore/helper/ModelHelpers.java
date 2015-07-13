fd44237f341f2a8a277075133cf10eeef3213800
Ruben Taelman <rubensworks@gmail.com>
2015-07-13 17:46:41 +0200

    Add empty list^2 for quads

--- src/main/java/org/cyclops/cyclopscore/helper/ModelHelpers.java
+++ src/main/java/org/cyclops/cyclopscore/helper/ModelHelpers.java
@@ -1,7 +1,9 @@
 package org.cyclops.cyclopscore.helper;
 
 import com.google.common.base.Charsets;
+import com.google.common.collect.Lists;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.ModelBlock;
 import net.minecraft.client.resources.IResource;
 import net.minecraft.util.ResourceLocation;
@@ -9,12 +11,23 @@ import net.minecraft.util.ResourceLocation;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Helpers for models.
  * @author rubensworks
  */
 public final class ModelHelpers {
+
+    // An empty list^2 for quads.
+    public static final List<List<BakedQuad>> EMPTY_FACE_QUADS;
+    static {
+        EMPTY_FACE_QUADS = Lists.newArrayList();
+        for(int i = 0; i < 6; i++) {
+            EMPTY_FACE_QUADS.add(Collections.<BakedQuad>emptyList());
+        }
+    }
 
     /**
      * Read the given model location to a {@link net.minecraft.client.renderer.block.model.ModelBlock}.
