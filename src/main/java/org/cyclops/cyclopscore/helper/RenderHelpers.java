29845f05fc728ea937eafc7fe3866c22694d2c9a
Ruben Taelman <rubensworks@gmail.com>
2015-07-23 15:57:37 +0200

    Add render helpers to get baked models and add block hit effects

--- src/main/java/org/cyclops/cyclopscore/helper/RenderHelpers.java
+++ src/main/java/org/cyclops/cyclopscore/helper/RenderHelpers.java
@@ -1,11 +1,22 @@
 package org.cyclops.cyclopscore.helper;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.particle.EffectRenderer;
+import net.minecraft.client.particle.EntityDiggingFX;
+import net.minecraft.client.particle.EntityFX;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.resources.model.IBakedModel;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Random;
 
 /**
  * A helper for rendering.
@@ -14,6 +25,8 @@ import net.minecraftforge.fml.relauncher.SideOnly;
  */
 @SideOnly(Side.CLIENT)
 public class RenderHelpers {
+
+    private static final Random rand = new Random();
     
     /**
      * Bind a texture to the rendering engine.
@@ -71,6 +84,46 @@ public class RenderHelpers {
         int titleHeight = fontRenderer.FONT_HEIGHT;
         fontRenderer.drawString(string, Math.round((x + width / 2) / scale - titleLength / 2), Math.round(y / scale - titleHeight / 2), color);
         GlStateManager.popMatrix();
+    }
+
+    /**
+     * Retrieve the baked model from a given block state.
+     * @param blockState The block state.
+     * @return The corresponding baked model.
+     */
+    public static IBakedModel getBakedModel(IBlockState blockState) {
+        return Minecraft.getMinecraft().getRenderItem().getItemModelMesher().getModelManager().getBlockModelShapes().getModelForState(blockState);
+    }
+
+    /**
+     * A custom way of spawning block hit effects.
+     * @param effectRenderer The effect renderer.
+     * @param world The world.
+     * @param blockState The blockstate to render particles for.
+     * @param pos The position.
+     * @param side The hit side.
+     */
+    public static void addBlockHitEffects(EffectRenderer effectRenderer, World world, IBlockState blockState, BlockPos pos, EnumFacing side)  {
+        Block block = blockState.getBlock();
+        if (block.getRenderType() != -1) {
+            int i = pos.getX();
+            int j = pos.getY();
+            int k = pos.getZ();
+            float f = 0.1F;
+            double d0 = (double)i + rand.nextDouble() * (block.getBlockBoundsMaxX() - block.getBlockBoundsMinX() - (double)(f * 2.0F)) + (double)f + block.getBlockBoundsMinX();
+            double d1 = (double)j + rand.nextDouble() * (block.getBlockBoundsMaxY() - block.getBlockBoundsMinY() - (double)(f * 2.0F)) + (double)f + block.getBlockBoundsMinY();
+            double d2 = (double)k + rand.nextDouble() * (block.getBlockBoundsMaxZ() - block.getBlockBoundsMinZ() - (double)(f * 2.0F)) + (double)f + block.getBlockBoundsMinZ();
+
+            if (side == EnumFacing.DOWN)  d1 = (double)j + block.getBlockBoundsMinY() - (double)f;
+            if (side == EnumFacing.UP)    d1 = (double)j + block.getBlockBoundsMaxY() + (double)f;
+            if (side == EnumFacing.NORTH) d2 = (double)k + block.getBlockBoundsMinZ() - (double)f;
+            if (side == EnumFacing.SOUTH) d2 = (double)k + block.getBlockBoundsMaxZ() + (double)f;
+            if (side == EnumFacing.WEST)  d0 = (double)i + block.getBlockBoundsMinX() - (double)f;
+            if (side == EnumFacing.EAST)  d0 = (double)i + block.getBlockBoundsMaxX() + (double)f;
+
+            EntityFX fx = new EntityDiggingFX.Factory().getEntityFX(-1, world, d0, d1, d2, 0.0D, 0.0D, 0.0D, Block.getStateId(blockState));
+            effectRenderer.addEffect(fx);
+        }
     }
 
 }
