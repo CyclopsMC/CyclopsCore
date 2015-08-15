dea87693d0e794aa83c80562a995c7c9bcc50996
Ruben Taelman <rubensworks@gmail.com>
2015-08-15 21:07:23 +0200

    Add error and ok symbols

--- src/main/java/org/cyclops/cyclopscore/client/gui/image/Images.java
+++ src/main/java/org/cyclops/cyclopscore/client/gui/image/Images.java
@@ -25,6 +25,8 @@ public class Images {
     public static final Image CONFIG_BOARD = new Image(ICONS, 36, 18, 18, 18);
     public static final Image PICKAXE = new Image(ICONS, 54, 18, 18, 18);
     public static final Image SHOVEL_BROKEN = new Image(ICONS, 72, 18, 18, 18);
+    public static final Image ERROR = new Image(ICONS, 54, 0, 13, 12);
+    public static final Image OK = new Image(ICONS, 72, 0, 14, 12);
 
     /**
      * 0: default; 1: active; 2: disabled
