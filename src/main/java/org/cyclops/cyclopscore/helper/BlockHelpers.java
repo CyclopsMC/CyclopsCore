9a6f180d9a1a896233fa5e8baed39bf0cf961425
Ruben Taelman <rubensworks@gmail.com>
2015-09-10 20:02:59 +0200

    Add serialization methods for block states

--- src/main/java/org/cyclops/cyclopscore/helper/BlockHelpers.java
+++ src/main/java/org/cyclops/cyclopscore/helper/BlockHelpers.java
@@ -1,9 +1,14 @@
 package org.cyclops.cyclopscore.helper;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.property.IExtendedBlockState;
 import net.minecraftforge.common.property.IUnlistedProperty;
+import org.apache.commons.lang3.tuple.Pair;
 
 import javax.annotation.Nullable;
 
@@ -51,6 +56,50 @@ public final class BlockHelpers {
             return fallback;
         }
         return value;
+    }
+
+    /**
+     * Convert the given blockstate to a pair of blockname string and meta value.
+     * @param blockState The blockstate to serialize.
+     * @return The pair of the blockname and meta value.
+     */
+    public static Pair<String, Integer> serializeBlockState(IBlockState blockState) {
+        String blockName = Block.blockRegistry.getNameForObject(blockState.getBlock()).toString();
+        int meta = blockState.getBlock().getMetaFromState(blockState);
+        return Pair.of(blockName, meta);
+    }
+
+    /**
+     * Convert the given serialized blockstate to a blockstate instance.
+     * @param serializedBlockState The pair of the blockname and meta value.
+     * @return The resulting blockstate. Can be null if the referred block does not exist.
+     */
+    public static IBlockState deserializeBlockState(Pair<String, Integer> serializedBlockState) {
+        Block block = Block.getBlockFromName(serializedBlockState.getLeft());
+        if(block != null) {
+            return block.getStateFromMeta(serializedBlockState.getRight());
+        }
+        return null;
+    }
+
+    /**
+     * Get the blockstate from the given itemstack
+     * @param itemStack The itemstack
+     * @return The blockstate
+     */
+    public static IBlockState getBlockStateFromItemStack(ItemStack itemStack) {
+        Block block = ((ItemBlock) itemStack.getItem()).getBlock();
+        return block.getStateFromMeta(itemStack.getMetadata());
+    }
+
+    /**
+     * Get the itemstack from the given blockstate
+     * @param blockState The blockstate
+     * @return The itemstack
+     */
+    public static ItemStack getItemStackFromBlockState(IBlockState blockState) {
+        Item item = Item.getItemFromBlock(blockState.getBlock());
+        return new ItemStack(item, 1, blockState.getBlock().getMetaFromState(blockState));
     }
 
 }
