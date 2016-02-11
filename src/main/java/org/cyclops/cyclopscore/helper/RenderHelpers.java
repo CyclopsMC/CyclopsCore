3488f37c3abc7c845fcd61846dbd786b885b9d41
Ruben Taelman <rubensworks@gmail.com>
2016-02-11 22:08:03 +0100

    Port fluid context rendering from EvilCraft

--- src/main/java/org/cyclops/cyclopscore/helper/RenderHelpers.java
+++ src/main/java/org/cyclops/cyclopscore/helper/RenderHelpers.java
@@ -14,9 +14,11 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.resources.model.IBakedModel;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
@@ -26,6 +28,7 @@ import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.opengl.GL11;
 
 import java.util.Random;
 
@@ -218,6 +221,71 @@ public class RenderHelpers {
         }
 
         return icon;
+    }
+
+    /**
+     * Prepare a GL context for rendering fluids.
+     * @param fluid The fluid stack.
+     * @param x X
+     * @param y Y
+     * @param z Z
+     * @param render The actual fluid renderer.
+     */
+    public static void renderFluidContext(FluidStack fluid, double x, double y, double z, IFluidContextRender render) {
+        if(fluid != null && fluid.amount > 0) {
+            GlStateManager.pushMatrix();
+
+            // Make sure both sides are rendered
+            GlStateManager.enableBlend();
+            GlStateManager.disableCull();
+
+            // Correct color & lighting
+            GlStateManager.color(1, 1, 1, 1);
+            GlStateManager.disableLighting();
+            GlStateManager.enableBlend();
+            GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+
+            // Set to current relative player location
+            GlStateManager.translate(x, y, z);
+
+            // Set blockState textures
+            Minecraft.getMinecraft().getTextureManager().bindTexture(TextureMap.locationBlocksTexture);
+
+            render.renderFluid(fluid);
+
+            //GlStateManager.enableCull();
+            GlStateManager.enableLighting();
+            GlStateManager.disableBlend();
+            //GlStateManager.disableDepth();
+            GlStateManager.popMatrix();
+        }
+    }
+
+    /**
+     * Prepare a GL context for rendering fluids for tile entities.
+     * @param fluid The fluid stack.
+     * @param x X
+     * @param y Y
+     * @param z Z
+     * @param tile The tile.
+     * @param render The actual fluid renderer.
+     */
+    public static void renderTileFluidContext(final FluidStack fluid, final double x, final double y, final double z, final TileEntity tile, final IFluidContextRender render) {
+        renderFluidContext(fluid, x, y, z, render);
+    }
+
+    /**
+     * Runnable for {@link RenderHelpers#renderFluidContext(FluidStack, double, double, double, IFluidContextRender)}.
+     * @author rubensworks
+     */
+    public static interface IFluidContextRender {
+
+        /**
+         * Render the fluid.
+         * @param fluid The fluid stack.
+         */
+        public void renderFluid(FluidStack fluid);
+
     }
 
 }
