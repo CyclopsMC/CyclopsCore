df501eb4e374a206e562a5e94a485419ed5d5c4d
Ruben Taelman <rubensworks@gmail.com>
2016-03-22 19:50:15 +0100

    Add publicized biome properties class

--- src/main/java/org/cyclops/cyclopscore/config/configurable/ConfigurableBiome.java
+++ src/main/java/org/cyclops/cyclopscore/config/configurable/ConfigurableBiome.java
@@ -19,9 +19,17 @@ public class ConfigurableBiome extends BiomeGenBase implements IConfigurable {
      * @param eConfig Config for this enchantment.
      */
     protected ConfigurableBiome(BiomeConfig eConfig) {
-        super(new BiomeProperties(L10NHelpers.localize(eConfig.getUnlocalizedName())));
+        super(constructProperties(eConfig));
         this.setConfig(eConfig);
-        
+    }
+
+    protected ConfigurableBiome(BiomeProperties properties, BiomeConfig eConfig) {
+        super(properties);
+        this.setConfig(eConfig);
+    }
+
+    protected static Properties constructProperties(BiomeConfig eConfig) {
+        return new Properties(L10NHelpers.localize(eConfig.getUnlocalizedName()));
     }
     
     @SuppressWarnings("rawtypes")
@@ -32,6 +40,56 @@ public class ConfigurableBiome extends BiomeGenBase implements IConfigurable {
     @Override
     public ExtendedConfig<?> getConfig() {
         return eConfig;
+    }
+
+    /**
+     * An extension of {@link BiomeProperties} which has all its setters made public.
+     */
+    public static class Properties extends BiomeProperties {
+
+        public Properties(String nameIn) {
+            super(nameIn);
+        }
+
+        @Override
+        public Properties setTemperature(float temperatureIn) {
+            return (Properties) super.setTemperature(temperatureIn);
+        }
+
+        @Override
+        public Properties setRainfall(float rainfallIn) {
+            return (Properties) super.setRainfall(rainfallIn);
+        }
+
+        @Override
+        public Properties setBaseHeight(float baseHeightIn) {
+            return (Properties) super.setBaseHeight(baseHeightIn);
+        }
+
+        @Override
+        public Properties setHeightVariation(float heightVariationIn) {
+            return (Properties) super.setHeightVariation(heightVariationIn);
+        }
+
+        @Override
+        public Properties setRainDisabled() {
+            return (Properties) super.setRainDisabled();
+        }
+
+        @Override
+        public Properties setSnowEnabled() {
+            return (Properties) super.setSnowEnabled();
+        }
+
+        @Override
+        public Properties setWaterColor(int waterColorIn) {
+            return (Properties) super.setWaterColor(waterColorIn);
+        }
+
+        @Override
+        public Properties setBaseBiome(String nameIn) {
+            return (Properties) super.setBaseBiome(nameIn);
+        }
     }
 
 }
