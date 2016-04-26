47cf88afb3389613c45ddff2ad94e4bcb9be5a11
Ruben Taelman <rubensworks@gmail.com>
2016-04-26 21:26:10 +0200

    Add helper to transform RGB to BGR(A)

--- src/main/java/org/cyclops/cyclopscore/helper/ModelHelpers.java
+++ src/main/java/org/cyclops/cyclopscore/helper/ModelHelpers.java
@@ -7,6 +7,7 @@ import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.ModelBlock;
 import net.minecraft.client.resources.IResource;
 import net.minecraft.util.ResourceLocation;
+import org.apache.commons.lang3.tuple.Triple;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -40,5 +41,28 @@ public final class ModelHelpers {
                 new ResourceLocation(modelLocation.getResourceDomain(), modelLocation.getResourcePath() + ".json"));
         Reader reader = new InputStreamReader(resource.getInputStream(), Charsets.UTF_8);
         return ModelBlock.deserialize(reader);
+    }
+
+    /**
+     * Convert the given color from RGB encoding to BGR encoding.
+     * @param color The color in RGB
+     * @param alpha The alpha to apply
+     * @return The color in BGR
+     */
+    public static int rgbToBgra(int color, int alpha) {
+        Triple<Float, Float, Float> triple = Helpers.intToRGB(color);
+        // RGB to BGR
+        return Helpers.RGBAToInt(
+                (int) (float) (triple.getRight() * 255F), (int) (float) (triple.getMiddle() * 255F), (int) (float) (triple.getLeft() * 255F),
+                alpha);
+    }
+
+    /**
+     * Convert the given color from RGB encoding to BGR encoding.
+     * @param color The color in RGB
+     * @return The color in BGR
+     */
+    public static int rgbToBgr(int color) {
+        return rgbToBgra(color, 255);
     }
 }
