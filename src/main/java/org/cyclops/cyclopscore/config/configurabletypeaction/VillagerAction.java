77de822aa76c8670ddba0e1c69a60a91fd43ac15
Ruben Taelman <rubensworks@gmail.com>
2016-05-15 16:43:08 +0200

    First steps at restoring villagers

--- src/main/java/org/cyclops/cyclopscore/config/configurabletypeaction/VillagerAction.java
+++ src/main/java/org/cyclops/cyclopscore/config/configurabletypeaction/VillagerAction.java
@@ -1,9 +1,9 @@
 package org.cyclops.cyclopscore.config.configurabletypeaction;
 
 import net.minecraftforge.common.config.Configuration;
-import net.minecraftforge.fml.common.registry.VillagerRegistry;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import org.cyclops.cyclopscore.config.configurable.ConfigurableVillager;
 import org.cyclops.cyclopscore.config.extendedconfig.VillagerConfig;
-import org.cyclops.cyclopscore.init.ModBase;
 
 /**
  * The action used for {@link VillagerConfig}.
@@ -21,17 +21,10 @@ public class VillagerAction extends ConfigurableTypeAction<VillagerConfig>{
     public void postRun(VillagerConfig eConfig, Configuration config) {
         // Save the config inside the correct element
         eConfig.save();
-        
-        // Register
-        String name = eConfig.getMod()+ ":" + eConfig.getNamedId();
-        String resource = eConfig.getMod() + ":" + eConfig.getMod().getReferenceValue(ModBase.REFKEY_TEXTURE_PATH_SKINS) + eConfig.getNamedId() + ".png";
-        VillagerRegistry.VillagerProfession profession = new VillagerRegistry.VillagerProfession(name, resource);
-        VillagerRegistry.instance().register(profession);
 
-        // TODO: This is still being written in Forge!!!
-        
-        // Add trades
-        //VillagerRegistry.instance().registerVillageTradeHandler(eConfig.getId(), (ConfigurableVillager) eConfig.getSubInstance());
+        // Register
+        //register((ConfigurableVillager) eConfig.getSubInstance(), eConfig); // Don't call this, because VillagerRegistry.VillagerProfession already sets registry name
+        GameRegistry.register((ConfigurableVillager) eConfig.getSubInstance());
     }
 
 }
