fb284b66dadbb89e9d2acad0482ab31f7cb3234c
Ruben Taelman <rubensworks@gmail.com>
2016-09-16 21:30:46 +0200

    Add more capability helpers

--- src/main/java/org/cyclops/cyclopscore/helper/TileHelpers.java
+++ src/main/java/org/cyclops/cyclopscore/helper/TileHelpers.java
@@ -4,6 +4,7 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.Capability;
 import org.cyclops.cyclopscore.datastructure.DimPos;
 
@@ -44,6 +45,17 @@ public final class TileHelpers {
     /**
      * Safely get a capability from a tile.
      * @param dimPos The dimensional position of the block providing the tile entity.
+     * @param capability The capability.
+     * @param <C> The capability instance.
+     * @return The capability or null.
+     */
+    public static <C> C getCapability(DimPos dimPos, Capability<C> capability) {
+        return getCapability(dimPos.getWorld(), dimPos.getBlockPos(), null, capability);
+    }
+
+    /**
+     * Safely get a capability from a tile.
+     * @param dimPos The dimensional position of the block providing the tile entity.
      * @param side The side to get the capability from.
      * @param capability The capability.
      * @param <C> The capability instance.
@@ -51,6 +63,31 @@ public final class TileHelpers {
      */
     public static <C> C getCapability(DimPos dimPos, EnumFacing side, Capability<C> capability) {
         return getCapability(dimPos.getWorld(), dimPos.getBlockPos(), side, capability);
+    }
+
+    /**
+     * Safely get a capability from a tile.
+     * @param world The world.
+     * @param pos The position of the block of the tile entity providing the capability.
+     * @param side The side to get the capability from.
+     * @param capability The capability.
+     * @param <C> The capability instance.
+     * @return The capability or null.
+     */
+    public static <C> C getCapability(World world, BlockPos pos, EnumFacing side, Capability<C> capability) {
+        return getCapability((IBlockAccess) world, pos, side, capability);
+    }
+
+    /**
+     * Safely get a capability from a tile.
+     * @param world The world.
+     * @param pos The position of the block of the tile entity providing the capability.
+     * @param capability The capability.
+     * @param <C> The capability instance.
+     * @return The capability or null.
+     */
+    public static <C> C getCapability(IBlockAccess world, BlockPos pos, Capability<C> capability) {
+        return getCapability(world, pos, null, capability);
     }
 
     /**
