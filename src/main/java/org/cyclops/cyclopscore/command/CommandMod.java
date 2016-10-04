aeb86a40399213535bc6a36f5a76722789cf501a
Ruben Taelman <rubensworks@gmail.com>
2016-10-04 20:52:52 +0200

    Fix commands breaking help command

--- src/main/java/org/cyclops/cyclopscore/command/CommandMod.java
+++ src/main/java/org/cyclops/cyclopscore/command/CommandMod.java
@@ -75,7 +75,7 @@ public class CommandMod implements ICommand {
 
     @Override
     public int compareTo(ICommand o) {
-        return 0;
+        return this.getCommandName().compareTo(o.getCommandName());
     }
 
     @Override
