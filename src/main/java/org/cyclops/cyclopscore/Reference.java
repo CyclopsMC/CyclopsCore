f83bc48e1e59c0bae7f939a862dcbdc2f68551fc
Ruben Taelman <rubensworks@gmail.com>
2016-12-27 16:32:45 +0100

    Update to MC 1.11.2

--- src/main/java/org/cyclops/cyclopscore/Reference.java
+++ src/main/java/org/cyclops/cyclopscore/Reference.java
@@ -14,7 +14,7 @@ public final class Reference {
     public static final String MOD_BUILD_NUMBER = "@BUILD_NUMBER@";
     public static final String MOD_MC_VERSION = "@MC_VERSION@";
     public static final String GA_TRACKING_ID = "UA-65307010-1";
-    public static final String VERSION_URL = "https://raw.githubusercontent.com/CyclopsMC/Versions/master/1.9/CyclopsCore.txt";
+    public static final String VERSION_URL = "https://raw.githubusercontent.com/CyclopsMC/Versions/master/1.11/CyclopsCore.txt";
 
     // Mod ID's
     public static final String MOD_VANILLA = "Minecraft";
@@ -29,6 +29,6 @@ public final class Reference {
 
     // Dependencies
     public static final String MOD_DEPENDENCIES =
-              "required-after:forge@[13.19.1.2189,);";
+              "required-after:forge@[13.20.0.2201,);";
 
 }
