29ccb49e3db5fa8aa77cfdde8c2f0a005a4afda9
Ruben Taelman <rubensworks@gmail.com>
2017-04-25 21:30:01 +0200

    Add tooltips to infobook navigation buttons

--- src/main/java/org/cyclops/cyclopscore/infobook/GuiInfoBook.java
+++ src/main/java/org/cyclops/cyclopscore/infobook/GuiInfoBook.java
@@ -17,10 +17,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.cyclops.cyclopscore.CyclopsCore;
-import org.cyclops.cyclopscore.helper.Helpers;
-import org.cyclops.cyclopscore.helper.InventoryHelpers;
-import org.cyclops.cyclopscore.helper.L10NHelpers;
-import org.cyclops.cyclopscore.helper.MinecraftHelpers;
+import org.cyclops.cyclopscore.helper.*;
 import org.cyclops.cyclopscore.network.packet.RequestPlayerNbtPacket;
 import org.lwjgl.opengl.GL11;
 
@@ -179,6 +176,30 @@ public abstract class GuiInfoBook extends GuiScreen {
         for(int i = 0; i < getPages(); i++) {
             infoBook.getCurrentSection().postDrawScreen(this, left + getOffsetXForPageWithWidths(i), top + getPageYOffset(), width, getGuiHeight(), infoBook.getCurrentPage() + i, x, y);
         }
+
+        if (this.buttonNextPage.visible && RenderHelpers.isPointInButton(this.buttonNextPage, x, y)) {
+            drawTooltip(x, y, Lists.newArrayList(L10NHelpers.localize("infobook.cyclopscore.next_page")));
+        }
+        if (this.buttonPreviousPage.visible && RenderHelpers.isPointInButton(this.buttonPreviousPage, x, y)) {
+            drawTooltip(x, y, Lists.newArrayList(L10NHelpers.localize("infobook.cyclopscore.previous_page")));
+        }
+        if (this.buttonBack.visible && RenderHelpers.isPointInButton(this.buttonBack, x, y)) {
+            drawTooltip(x, y, Lists.newArrayList(L10NHelpers.localize("infobook.cyclopscore.last_page")));
+        }
+        if (this.buttonParent.visible && RenderHelpers.isPointInButton(this.buttonParent, x, y)) {
+            drawTooltip(x, y, Lists.newArrayList(L10NHelpers.localize("infobook.cyclopscore.parent_section")));
+        }
+    }
+
+    public void drawTooltip(int mx, int my, List<String> lines) {
+        GlStateManager.pushMatrix();
+        drawHoveringText(lines, mx, my);
+        GlStateManager.popMatrix();
+
+        GlStateManager.disableLighting();
+
+        GlStateManager.enableBlend();
+        GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
     }
 
     public void drawTexturedModalRectMirrored(int x, int y, int u, int v, int width, int height) {
