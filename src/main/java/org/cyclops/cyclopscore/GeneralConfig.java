c18cfd67c85719ca1d904f162e0db9d44e28ab1e
Ruben Taelman <rubensworks@gmail.com>
2017-05-05 19:10:19 +0200

    Remove config outdated warning, Closes #50

--- src/main/java/org/cyclops/cyclopscore/GeneralConfig.java
+++ src/main/java/org/cyclops/cyclopscore/GeneralConfig.java
@@ -1,6 +1,5 @@
 package org.cyclops.cyclopscore;
 
-import org.apache.logging.log4j.Level;
 import org.cyclops.cyclopscore.config.ConfigurableProperty;
 import org.cyclops.cyclopscore.config.ConfigurableType;
 import org.cyclops.cyclopscore.config.ConfigurableTypeCategory;
@@ -19,10 +18,9 @@ import java.util.UUID;
 public class GeneralConfig extends DummyConfig {
     
     /**
-     * The current mod version, will be used to check if the player's config isn't out of date and
-     * warn the player accordingly.
+     * The version at which the config file was generated.
      */
-    @ConfigurableProperty(category = ConfigurableTypeCategory.CORE, comment = "Config version for " + Reference.MOD_NAME +".\nDO NOT EDIT MANUALLY!", showInGui = false)
+    @ConfigurableProperty(category = ConfigurableTypeCategory.CORE, comment = "The version at which the config file was generated.", showInGui = false)
     public static String version = Reference.MOD_VERSION;
     
     /**
@@ -81,11 +79,6 @@ public class GeneralConfig extends DummyConfig {
     
     @Override
     public void onRegistered() {
-        // Check version of config file
-        if(!version.equals(Reference.MOD_VERSION)) {
-            getMod().log(Level.WARN, "The config file of " + Reference.MOD_NAME + " is out of date and might cause problems, please remove it so it can be regenerated.");
-        }
-
         getMod().putGenericReference(ModBase.REFKEY_CRASH_ON_INVALID_RECIPE, GeneralConfig.crashOnInvalidRecipe);
         getMod().putGenericReference(ModBase.REFKEY_DEBUGCONFIG, GeneralConfig.debug);
         getMod().putGenericReference(ModBase.REFKEY_CRASH_ON_MODCOMPAT_CRASH, GeneralConfig.crashOnModCompatCrash);
