7dc69b87a232033c50a0d80d1d2e7b297b791be8
Ruben Taelman <rubensworks@gmail.com>
2017-05-07 06:50:04 +0200

    Add configurable stairs block

---
+++ src/main/java/org/cyclops/cyclopscore/config/configurable/ConfigurableBlockStairs.java
@@ -0,0 +1,90 @@
+package org.cyclops.cyclopscore.config.configurable;
+
+import net.minecraft.block.BlockStairs;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.color.IBlockColor;
+import net.minecraftforge.client.event.ModelBakeEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.cyclops.cyclopscore.client.model.IDynamicModelElement;
+import org.cyclops.cyclopscore.config.extendedconfig.BlockConfig;
+import org.cyclops.cyclopscore.config.extendedconfig.ExtendedConfig;
+import org.cyclops.cyclopscore.helper.MinecraftHelpers;
+
+import javax.annotation.Nullable;
+
+/**
+ * Stairs block that can hold ExtendedConfigs
+ * @author rubensworks
+ *
+ */
+public class ConfigurableBlockStairs extends BlockStairs implements IConfigurableBlock, IDynamicModelElement {
+
+    @SuppressWarnings("rawtypes")
+    protected BlockConfig eConfig = null;
+    protected boolean hasGui = false;
+
+    /**
+     * Make a new blockState instance.
+     * @param eConfig Config for this blockState.
+     * @param modelState The base model of the stairs
+     */
+    @SuppressWarnings({ "rawtypes" })
+    public ConfigurableBlockStairs(ExtendedConfig eConfig, IBlockState modelState) {
+        super(modelState);
+        this.setConfig(eConfig);
+        this.setUnlocalizedName(eConfig.getUnlocalizedName());
+        if(MinecraftHelpers.isClientSide() && hasDynamicModel()) MinecraftForge.EVENT_BUS.register(this);
+        this.useNeighborBrightness = true;
+    }
+
+    @Override
+    public boolean hasGui() {
+        return hasGui;
+    }
+
+    @Nullable
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IBlockColor getBlockColorHandler() {
+        return null;
+    }
+
+    private void setConfig(@SuppressWarnings("rawtypes") ExtendedConfig eConfig) {
+        this.eConfig = (BlockConfig) eConfig;
+    }
+
+    @Override
+    public ExtendedConfig<?> getConfig() {
+        return eConfig;
+    }
+
+    @Override
+    public boolean hasDynamicModel() {
+        return false;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public IBakedModel createDynamicModel() {
+        return null;
+    }
+
+    /**
+     * Called for baking the model of this cable depending on its state.
+     * @param event The bake event.
+     */
+    @SideOnly(Side.CLIENT)
+    @SubscribeEvent
+    public void onModelBakeEvent(ModelBakeEvent event){
+        if(hasDynamicModel()) {
+            IBakedModel cableModel = createDynamicModel();
+            event.getModelRegistry().putObject(eConfig.dynamicBlockVariantLocation, cableModel);
+            event.getModelRegistry().putObject(eConfig.dynamicItemVariantLocation , cableModel);
+        }
+    }
+
+}
