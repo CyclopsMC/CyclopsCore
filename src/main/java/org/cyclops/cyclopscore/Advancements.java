ddff259ab7981bf9c784494ac97e0c65d5ecdda8
Ruben Taelman <rubensworks@gmail.com>
2017-07-04 19:53:44 +0200

    Add criterion trigger for obtaining a mod item

--- src/main/java/org/cyclops/cyclopscore/Advancements.java
+++ src/main/java/org/cyclops/cyclopscore/Advancements.java
@@ -1,6 +1,7 @@
 package org.cyclops.cyclopscore;
 
 import org.cyclops.cyclopscore.advancement.criterion.ItemCraftedTrigger;
+import org.cyclops.cyclopscore.advancement.criterion.ModItemObtainedTrigger;
 import org.cyclops.cyclopscore.helper.AdvancementHelpers;
 
 /**
@@ -11,6 +12,8 @@ public class Advancements {
 
     public static final ItemCraftedTrigger ITEM_CRAFTED = AdvancementHelpers
             .registerCriteriaTrigger(new ItemCraftedTrigger());
+    public static final ModItemObtainedTrigger MOD_ITEM_OBTAINED = AdvancementHelpers
+            .registerCriteriaTrigger(new ModItemObtainedTrigger());
 
     public static void load() {}
 
---
+++ src/main/java/org/cyclops/cyclopscore/advancement/criterion/ModItemObtainedTrigger.java
@@ -0,0 +1,66 @@
+package org.cyclops.cyclopscore.advancement.criterion;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonObject;
+import net.minecraft.advancements.critereon.AbstractCriterionInstance;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import org.cyclops.cyclopscore.Reference;
+
+/**
+ * Trigger for when an item of a given mod is obtained.
+ * @author rubensworks
+ */
+public class ModItemObtainedTrigger extends BaseCriterionTrigger<ItemStack, ModItemObtainedTrigger.Instance> {
+    public ModItemObtainedTrigger() {
+        super(new ResourceLocation(Reference.MOD_ID, "mod_item_obtained"));
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @Override
+    public Instance deserializeInstance(JsonObject json, JsonDeserializationContext context) {
+        return new Instance(getId(), json.get("mod_id").getAsString());
+    }
+
+    @SubscribeEvent
+    public void onPickup(PlayerEvent.ItemPickupEvent event) {
+        if (event.player != null && event.player instanceof EntityPlayerMP) {
+            this.trigger((EntityPlayerMP) event.player, event.pickedUp.getItem());
+        }
+    }
+
+    @SubscribeEvent
+    public void onCrafted(PlayerEvent.ItemCraftedEvent event) {
+        if (event.player != null && event.player instanceof EntityPlayerMP) {
+            this.trigger((EntityPlayerMP) event.player, event.crafting);
+        }
+    }
+
+    @SubscribeEvent
+    public void onSmelted(PlayerEvent.ItemSmeltedEvent event) {
+        if (event.player != null && event.player instanceof EntityPlayerMP) {
+            this.trigger((EntityPlayerMP) event.player, event.smelting);
+        }
+    }
+
+    public static class Instance extends AbstractCriterionInstance implements ICriterionInstanceTestable<ItemStack> {
+        private final String modId;
+
+        public Instance(ResourceLocation criterionIn, String modId) {
+            super(criterionIn);
+            this.modId = modId;
+        }
+
+        @Override
+        public boolean test(EntityPlayerMP player, ItemStack itemStack) {
+            return !itemStack.isEmpty()
+                    && Item.REGISTRY.getNameForObject(itemStack.getItem()).getResourceDomain().equals(this.modId);
+        }
+    }
+
+}
