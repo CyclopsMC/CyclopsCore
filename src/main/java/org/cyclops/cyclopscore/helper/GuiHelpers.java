bf5a7728d1b3fd9adaa84696fc35ac989d2b9857
Ruben Taelman <rubensworks@gmail.com>
2017-11-24 16:18:00 +0900

    Add helper for rendering a progress bar in a gui

--- src/main/java/org/cyclops/cyclopscore/helper/GuiHelpers.java
+++ src/main/java/org/cyclops/cyclopscore/helper/GuiHelpers.java
@@ -75,4 +75,80 @@ public class GuiHelpers {
         }
     }
 
+    /**
+     * Render a progress bar in a certain direction.
+     * The currently bound texture will be used to render the progress bar.
+     *
+     * @param gui The gui to render in.
+     * @param x The gui x position, including gui left.
+     * @param y The gui y position, including gui top.
+     * @param width The progress bar width.
+     * @param height The progress bar height.
+     * @param textureX The texture x position.
+     * @param textureY The texture y position.
+     * @param direction The direction to progress in.
+     * @param progress The current progress.
+     * @param progressMax The maximum progress.
+     */
+    public static void renderProgressBar(Gui gui, int x, int y, int width, int height, int textureX, int textureY,
+                                         ProgressDirection direction, int progress, int progressMax) {
+        if (progressMax > 0 && progress > 0) {
+            int scaledWidth = width;
+            int scaledHeight = height;
+
+            // Scale the width and/or height
+            if (direction.getIncrementX() != 0) {
+                scaledWidth = scaledWidth * progress / progressMax;
+            }
+            if (direction.getIncrementY() != 0) {
+                scaledHeight = scaledHeight * progress / progressMax;
+            }
+
+            // If increments happen inversely, make sure we start incrementing from the other end of the progress bar
+            if (direction.getIncrementX() < 0) {
+                int offset = width - scaledWidth;
+                x += offset;
+                textureX += offset;
+            }
+            if (direction.getIncrementY() < 0) {
+                int offset = height - scaledHeight;
+                y += offset;
+                textureY += offset;
+            }
+
+            gui.drawTexturedModalRect(x, y, textureX, textureY, scaledWidth, scaledHeight);
+        }
+    }
+
+    /**
+     * Represents the direction of a progress bar.
+     */
+    public static enum ProgressDirection {
+        UP(0, -1),
+        DOWN(0, 1),
+        LEFT(-1, 0),
+        RIGHT(1, 0),
+
+        UP_LEFT(-1, -1),
+        UP_RIGHT(1, -1),
+        DOWN_LEFT(-1, 1),
+        DOWN_RIGHT(1, 1);
+
+        private final int incrementX;
+        private final int incrementY;
+
+        private ProgressDirection(int incrementX, int incrementY) {
+            this.incrementX = incrementX;
+            this.incrementY = incrementY;
+        }
+
+        public int getIncrementX() {
+            return incrementX;
+        }
+
+        public int getIncrementY() {
+            return incrementY;
+        }
+    }
+
 }
