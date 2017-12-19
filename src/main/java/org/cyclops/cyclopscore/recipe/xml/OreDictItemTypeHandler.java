0239703c7cdc6b3fe59c80f904dc3d68b5e72863
Ruben Taelman <rubensworks@gmail.com>
2017-12-19 16:05:57 +0100

    Fix XML recipe oredict ingredients not adhering to stacksizes

--- src/main/java/org/cyclops/cyclopscore/recipe/xml/OreDictItemTypeHandler.java
+++ src/main/java/org/cyclops/cyclopscore/recipe/xml/OreDictItemTypeHandler.java
@@ -1,8 +1,12 @@
 package org.cyclops.cyclopscore.recipe.xml;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraftforge.oredict.OreIngredient;
 import org.cyclops.cyclopscore.init.RecipeHandler;
+
+import javax.annotation.Nonnull;
+import java.util.Arrays;
 
 /**
  * Item type handler for ore dictionary keys.
@@ -15,7 +19,20 @@ public class OreDictItemTypeHandler extends DefaultItemTypeHandler {
         if (nbtSensitive) {
             throw new IllegalArgumentException("NBT-sensitivity can not be defined on oredict ingredients: " + key);
         }
-        return new OreIngredient(key);
+        return new OreIngredient(key) {
+            @Nonnull
+            @Override
+            public ItemStack[] getMatchingStacks() {
+                if (amount > 0) {
+                    return Arrays.stream(super.getMatchingStacks()).map(stack -> {
+                        stack = stack.copy();
+                        stack.setCount(amount);
+                        return stack;
+                    }).toArray(ItemStack[]::new);
+                }
+                return super.getMatchingStacks();
+            }
+        };
     }
 	
 }
