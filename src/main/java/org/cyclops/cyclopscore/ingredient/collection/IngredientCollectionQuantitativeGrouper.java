5e7f7edcf61839bfe02918824f0e7f68bafc1ba6
Ruben Taelman <rubensworks@gmail.com>
2018-05-28 19:30:21 +0200

    Add quantitative grouper ingredient collection

---
+++ src/main/java/org/cyclops/cyclopscore/ingredient/collection/IngredientCollectionQuantitativeGrouper.java
@@ -0,0 +1,220 @@
+package org.cyclops.cyclopscore.ingredient.collection;
+
+import com.google.common.collect.Lists;
+import org.cyclops.commoncapabilities.api.ingredient.IIngredientMatcher;
+import org.cyclops.commoncapabilities.api.ingredient.IngredientComponentCategoryType;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * A mutable ingredient collection that wraps over another mutable ingredient collection
+ * and groups instances by stacking them using their primary quantifier.
+ *
+ * Warning: The given collection must allow the storage of multiple equal instances.
+ * List-based collections for example are ideal.
+ *
+ * Warning: If the given collection is mutated outside of this grouper,
+ * behaviour becomes undefined.
+ *
+ * @param <T> An instance type.
+ * @param <M> The matching condition parameter.
+ */
+public class IngredientCollectionQuantitativeGrouper<T, M>
+        extends IngredientCollectionMutableWrapper<T, M, IIngredientCollectionMutable<T, M>> {
+
+    private final IngredientComponentCategoryType<T, M, ?> primaryQuantifier;
+    private final M quantifierlessMatchCondition;
+    private final long maxQuantity;
+
+    public IngredientCollectionQuantitativeGrouper(IIngredientCollectionMutable<T, M> innerCollection,
+                                                   boolean ignoreInnerCollectionEmptyCheck) {
+        super(innerCollection);
+
+        // Safety check to see if the given collection is empty.
+        if (!ignoreInnerCollectionEmptyCheck && !innerCollection.isEmpty()) {
+            throw new IllegalArgumentException("Inner collections must be empty before grouping.");
+        }
+
+        this.primaryQuantifier = getComponent().getPrimaryQuantifier();
+        if (this.primaryQuantifier == null) {
+            throw new IllegalArgumentException("Quantitative grouping requires a primary quantifier on the component type.");
+        }
+        IIngredientMatcher<T, M> matcher = getComponent().getMatcher();
+        this.quantifierlessMatchCondition = matcher.withoutCondition(matcher.getExactMatchCondition(),
+                getPrimaryQuantifier().getMatchCondition());
+        this.maxQuantity = matcher.getMaximumQuantity();
+    }
+
+    public IngredientCollectionQuantitativeGrouper(IIngredientCollectionMutable<T, M> innerCollection) {
+        this(innerCollection, false);
+    }
+
+    protected IngredientComponentCategoryType<T, M, ?> getPrimaryQuantifier() {
+        return primaryQuantifier;
+    }
+
+    protected M getQuantifierlessMatchCondition() {
+        return quantifierlessMatchCondition;
+    }
+
+    @Override
+    public boolean contains(T instance) {
+        Iterator<T> it = iterator(instance, getQuantifierlessMatchCondition());
+        if (!it.hasNext()) {
+            // Fail if there are no matches
+            return false;
+        }
+        T first = it.next();
+        if (it.hasNext()) {
+            // If we have more than one matches, at least one of them has the max quantity
+            return true;
+        }
+        // Otherwise, check if the single existing instance has a quantity
+        // that is equal or higher than the given instance quantity.
+        IIngredientMatcher<T, M> matcher = getComponent().getMatcher();
+        return matcher.getQuantity(instance) <= matcher.getQuantity(first);
+    }
+
+    @Override
+    public boolean containsAll(Iterable<? extends T> instances) {
+        for (T instance : instances) {
+            if (!contains(instance)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean add(T instance) {
+        // If an equal instance already exists, add its quantity
+        Iterator<T> it = iterator(instance, getQuantifierlessMatchCondition());
+        while (it.hasNext()) {
+            IIngredientMatcher<T, M> matcher = getComponent().getMatcher();
+            T existing = it.next();
+
+            // Check if the combined amount will be within the accepted amount.
+            long remainingAllowedQuantity = maxQuantity - matcher.getQuantity(existing);
+            long newQuantity = matcher.getQuantity(instance);
+            if (remainingAllowedQuantity == 0) {
+                // This instance reached its max quantity
+                // continue;
+            } else if (remainingAllowedQuantity >= newQuantity) {
+                // If so, combine both instance quantities
+
+                // Remove the existing instance, as we consider instances as being immutable
+                it.remove();
+
+                long combinedQuantity = Math.addExact(matcher.getQuantity(existing), matcher.getQuantity(instance));
+                T joinedInstance = matcher.withQuantity(instance, combinedQuantity);
+                return super.add(joinedInstance);
+            } else {
+                // If not, split up into two separate instances where we fill the first one to the maximum amount.
+
+                // Remove the existing instance, as we consider instances as being immutable
+                it.remove();
+
+                long remainingQuantity = newQuantity - remainingAllowedQuantity;
+                return super.add(matcher.withQuantity(existing, maxQuantity))
+                        & super.add(matcher.withQuantity(instance, remainingQuantity));
+            }
+        }
+        return super.add(instance);
+    }
+
+    @Override
+    public boolean addAll(Iterable<? extends T> instances) {
+        boolean changed = false;
+        for (T instance : instances) {
+            changed |= add(instance);
+        }
+        return changed;
+    }
+
+    @Override
+    public boolean remove(T instance) {
+        // If the instance already exists,
+        // and its quantity is equal or higher than the given instance quantity,
+        // subtract its quantity
+        List<T> existingInstances = Lists.newArrayList(iterator(instance, getQuantifierlessMatchCondition()));
+        if (existingInstances.isEmpty()) {
+            return false;
+        }
+
+        // Sort matching instances by increasing quantities
+        IIngredientMatcher<T, M> matcher = getComponent().getMatcher();
+        existingInstances.sort((o1, o2) -> (int) (matcher.getQuantity(o1) - matcher.getQuantity(o2)));
+        boolean hasMaxInstance = existingInstances.size() > 1; // If at least one instance with max quantity exists
+
+        // Iterate through existing instances and remove until the desired amount is reached
+        long toSubtract = matcher.getQuantity(instance);
+        Iterator<T> it = existingInstances.iterator();
+        while (it.hasNext()) {
+            T existing = it.next();
+
+            long existingQuantity = matcher.getQuantity(existing);
+            if (existingQuantity > toSubtract) {
+                // Remove the existing instance, as we consider instances as being immutable
+                super.remove(existing);
+                // Add a new instance with the reduced amount
+                super.add(matcher.withQuantity(instance, existingQuantity - toSubtract));
+                return true;
+            } else if (existingQuantity == toSubtract) {
+                // Remove the existing instance, as we consider instances as being immutable
+                super.remove(existing);
+                // Don't remove a new instance, as everything was removed
+                return true;
+            } else if (!hasMaxInstance) {
+                // Fail if the given instance quantity was larger than the existing instance quantity,
+                // and we have no max instance.
+                return false;
+            } else {
+                // Remove the existing instance, as we consider instances as being immutable
+                super.remove(existing);
+                // Reduce what we have to subtract with as much as we can with the current existing instance.
+                // The next iteration is guaranteed to be able to handle the remaining quantity.
+                toSubtract -= existingQuantity;
+            }
+        }
+        throw new IllegalStateException("Failed to remove due to invalid instance grouping: " + existingInstances);
+    }
+
+    @Override
+    public int removeAll(T instance, M matchCondition) {
+        Iterator<T> it = this.iterator(instance, matchCondition);
+        int count = 0;
+        while (it.hasNext()) {
+            it.next();
+            it.remove();
+            count++;
+        }
+        return count;
+    }
+
+    @Override
+    public int removeAll(Iterable<? extends T> instances) {
+        int removed = 0;
+        for (T instance : instances) {
+            if (remove(instance)) {
+                removed++;
+            }
+        }
+        return removed;
+    }
+
+    @Override
+    public int removeAll(Iterable<? extends T> instances, M matchCondition) {
+        if (Objects.equals(getComponent().getMatcher().getAnyMatchCondition(), matchCondition)) {
+            int size = size();
+            this.clear();
+            return size;
+        }
+        int removed = 0;
+        for (T instance : instances) {
+            removed += removeAll(instance, matchCondition);
+        }
+        return removed;
+    }
+}
---
+++ src/test/java/org/cyclops/cyclopscore/ingredient/collection/TestIngredientCollectionQuantitativeGrouper.java
@@ -0,0 +1,346 @@
+package org.cyclops.cyclopscore.ingredient.collection;
+
+import com.google.common.collect.Lists;
+import net.minecraft.util.ResourceLocation;
+import org.cyclops.commoncapabilities.api.ingredient.IngredientComponent;
+import org.cyclops.commoncapabilities.api.ingredient.IngredientComponentCategoryType;
+import org.cyclops.cyclopscore.ingredient.ComplexStack;
+import org.cyclops.cyclopscore.ingredient.IngredientComponentStubs;
+import org.cyclops.cyclopscore.ingredient.IngredientMatcherComplex;
+import org.cyclops.cyclopscore.ingredient.IngredientSerializerStub;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestIngredientCollectionQuantitativeGrouper {
+
+    private static final ComplexStack CA01_ = new ComplexStack(ComplexStack.Group.A, 0, 1, null);
+    private static final ComplexStack CA02_ = new ComplexStack(ComplexStack.Group.A, 0, 2, null);
+    private static final ComplexStack CA05_ = new ComplexStack(ComplexStack.Group.A, 0, 5, null);
+    private static final ComplexStack CA0MAX_ = new ComplexStack(ComplexStack.Group.A, 0, Integer.MAX_VALUE, null);
+    private static final ComplexStack CA0MAXM_ = new ComplexStack(ComplexStack.Group.A, 0, Integer.MAX_VALUE - 1, null);
+    private static final ComplexStack CB02_ = new ComplexStack(ComplexStack.Group.B, 0, 2, null);
+    private static final ComplexStack CA91B = new ComplexStack(ComplexStack.Group.A, 9, 1, ComplexStack.Tag.B);
+    private static final ComplexStack CA01B = new ComplexStack(ComplexStack.Group.A, 0, 1, ComplexStack.Tag.B);
+
+    private IIngredientCollectionMutable<ComplexStack, Integer> collection;
+
+    @Before
+    public void beforeEach() {
+        this.collection = new IngredientCollectionQuantitativeGrouper<>(new IngredientArrayList<>(IngredientComponentStubs.COMPLEX));
+    }
+
+    @Test
+    public void testConstruct() {
+        assertThat(new IngredientCollectionQuantitativeGrouper<>(new IngredientArrayList<>(IngredientComponentStubs.COMPLEX)), instanceOf(IngredientCollectionQuantitativeGrouper.class));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructNonEmpty() {
+        IngredientArrayList<ComplexStack, Integer> c = new IngredientArrayList<>(IngredientComponentStubs.COMPLEX);
+        c.add(CA01_);
+        new IngredientCollectionQuantitativeGrouper<>(c);
+    }
+
+    @Test
+    public void testConstructNoCheck() {
+        IngredientArrayList<ComplexStack, Integer> c = new IngredientArrayList<>(IngredientComponentStubs.COMPLEX);
+        c.add(CA01_);
+        assertThat(new IngredientCollectionQuantitativeGrouper<>(new IngredientArrayList<>(IngredientComponentStubs.COMPLEX), true), instanceOf(IngredientCollectionQuantitativeGrouper.class));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructNoPrimaryQuantifier() {
+        IngredientComponent<ComplexStack, Integer> comp =
+                new IngredientComponent<>("cyclopscore:complex", new IngredientMatcherComplex(),
+                        new IngredientSerializerStub<>(), Lists.newArrayList(
+                        new IngredientComponentCategoryType<>(new ResourceLocation("cyclopscore:complex/group"),
+                                ComplexStack.Group.class, true, ComplexStack::getGroup, ComplexStack.Match.GROUP, false),
+                        new IngredientComponentCategoryType<>(new ResourceLocation("cyclopscore:complex/meta"),
+                                Integer.class, false, ComplexStack::getMeta, ComplexStack.Match.META, false),
+                        new IngredientComponentCategoryType<>(new ResourceLocation("cyclopscore:complex/tag"),
+                                ComplexStack.Tag.class, true, ComplexStack::getTag, ComplexStack.Match.TAG | ComplexStack.Match.AMOUNT, false)
+                )).setUnlocalizedName("recipecomponent.cyclopscore.complex");
+        new IngredientCollectionQuantitativeGrouper<>(new IngredientHashSet<>(comp));
+    }
+
+    @Test
+    public void testAddSingle() {
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.size(), is(1));
+    }
+
+    @Test
+    public void testAddMultipleDifferent() {
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.add(CB02_), is(true));
+        assertThat(this.collection.size(), is(2));
+    }
+
+    @Test
+    public void testAddMultipleEqual() {
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.size(), is(1));
+    }
+
+    @Test
+    public void testAddReachMaxQuantity() {
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.add(CA0MAXM_), is(true));
+        assertThat(this.collection.size(), is(1));
+        assertThat(this.collection.contains(CA0MAX_), is(true));
+    }
+
+    @Test
+    public void testAddBreachMaxQuantity() {
+        assertThat(this.collection.add(CA0MAX_), is(true));
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.size(), is(2));
+        assertThat(this.collection.contains(CA0MAX_), is(true));
+        assertThat(this.collection.contains(CA01_), is(true));
+        assertThat(this.collection.contains(CA05_), is(true));
+    }
+
+    @Test
+    public void testAddBreachMaxQuantityAlt() {
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.add(CA0MAXM_), is(true));
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.size(), is(2));
+        assertThat(this.collection.contains(CA0MAX_), is(true));
+        assertThat(this.collection.contains(CA01_), is(true));
+        assertThat(this.collection.contains(CA05_), is(true));
+    }
+
+    @Test
+    public void testAddBreachMaxQuantity2() {
+        assertThat(this.collection.add(CA0MAX_), is(true));
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.add(CA0MAX_), is(true));
+        assertThat(this.collection.size(), is(3));
+        assertThat(this.collection.contains(CA0MAX_), is(true));
+        assertThat(this.collection.contains(CA01_), is(true));
+        assertThat(this.collection.contains(CA05_), is(true));
+    }
+
+    @Test
+    public void testAddBreachMaxQuantityAlt2() {
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.add(CA0MAXM_), is(true));
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.add(CA0MAXM_), is(true));
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.size(), is(3));
+        assertThat(this.collection.contains(CA0MAX_), is(true));
+        assertThat(this.collection.contains(CA01_), is(true));
+        assertThat(this.collection.contains(CA05_), is(true));
+    }
+
+    @Test
+    public void testAddAllDifferemt() {
+        assertThat(this.collection.addAll(Lists.newArrayList(CA01_)), is(true));
+        assertThat(this.collection.addAll(Lists.newArrayList(CB02_)), is(true));
+        assertThat(this.collection.addAll(Lists.newArrayList(CA91B)), is(true));
+        assertThat(this.collection.size(), is(3));
+    }
+
+    @Test
+    public void testAddAllSame() {
+        assertThat(this.collection.addAll(Lists.newArrayList(CA01_)), is(true));
+        assertThat(this.collection.addAll(Lists.newArrayList(CA01_)), is(true));
+        assertThat(this.collection.addAll(Lists.newArrayList(CA01_)), is(true));
+        assertThat(this.collection.addAll(Lists.newArrayList(CA01_)), is(true));
+        assertThat(this.collection.addAll(Lists.newArrayList(CA01_)), is(true));
+        assertThat(this.collection.size(), is(1));
+        assertThat(this.collection.contains(CA05_), is(true));
+    }
+
+    @Test
+    public void testContainsSingle() {
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.contains(CA01_), is(true));
+        assertThat(this.collection.contains(CA02_), is(false));
+        assertThat(this.collection.contains(CB02_), is(false));
+    }
+
+    @Test
+    public void testContainsMultiple() {
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.contains(CA01_), is(true));
+        assertThat(this.collection.contains(CA02_), is(true));
+        assertThat(this.collection.contains(CA05_), is(false));
+    }
+
+    @Test
+    public void testContainsReachedMaxQuantity() {
+        assertThat(this.collection.add(CA0MAX_), is(true));
+        assertThat(this.collection.contains(CA01_), is(true));
+        assertThat(this.collection.contains(CA0MAX_), is(true));
+    }
+
+    @Test
+    public void testContainsBreachedMaxQuantity() {
+        assertThat(this.collection.add(CA0MAX_), is(true));
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.contains(CA01_), is(true));
+        assertThat(this.collection.contains(CA0MAX_), is(true));
+    }
+
+    @Test
+    public void testContainsAll() {
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.containsAll(Lists.newArrayList(CA01_, CA02_)), is(true));
+        assertThat(this.collection.containsAll(Lists.newArrayList(CA01_, CA05_)), is(false));
+    }
+
+    @Test
+    public void testRemoveSingle() {
+        assertThat(this.collection.remove(CA01_), is(false));
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.size(), is(1));
+        assertThat(this.collection.remove(CA01_), is(true));
+        assertThat(this.collection.size(), is(0));
+    }
+
+    @Test
+    public void testRemoveMultiple() {
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.size(), is(1));
+
+        assertThat(this.collection.remove(CA01_), is(true));
+        assertThat(this.collection.size(), is(1));
+
+        assertThat(this.collection.remove(CA01_), is(true));
+        assertThat(this.collection.size(), is(1));
+
+        assertThat(this.collection.remove(CA01_), is(true));
+        assertThat(this.collection.size(), is(0));
+    }
+
+    @Test
+    public void testRemoveReachedMaxQuantity() {
+        assertThat(this.collection.add(CA0MAX_), is(true));
+
+        assertThat(this.collection.remove(CA0MAXM_), is(true));
+        assertThat(this.collection.size(), is(1));
+
+        assertThat(this.collection.remove(CA01_), is(true));
+        assertThat(this.collection.size(), is(0));
+    }
+
+    @Test
+    public void testRemoveBreachedMaxQuantity() {
+        assertThat(this.collection.add(CA0MAX_), is(true));
+        assertThat(this.collection.add(CA01_), is(true));
+
+        assertThat(this.collection.remove(CA0MAX_), is(true));
+        assertThat(this.collection.size(), is(1));
+
+        assertThat(this.collection.remove(CA01_), is(true));
+        assertThat(this.collection.size(), is(0));
+    }
+
+    @Test
+    public void testRemoveBreached2MaxQuantity() {
+        assertThat(this.collection.add(CA0MAX_), is(true));
+        assertThat(this.collection.add(CA0MAX_), is(true));
+        assertThat(this.collection.add(CA01_), is(true));
+
+        assertThat(this.collection.remove(CA0MAX_), is(true));
+        assertThat(this.collection.size(), is(2));
+
+        assertThat(this.collection.remove(CA0MAX_), is(true));
+        assertThat(this.collection.size(), is(1));
+
+        assertThat(this.collection.remove(CA01_), is(true));
+        assertThat(this.collection.size(), is(0));
+    }
+
+    @Test
+    public void testRemoveFailTooLarge() {
+        assertThat(this.collection.add(CA01_), is(true));
+        assertThat(this.collection.size(), is(1));
+
+        assertThat(this.collection.remove(CA05_), is(false));
+        assertThat(this.collection.size(), is(1));
+    }
+
+    @Test
+    public void testRemoveAllExact() {
+        collection.add(CA01_);
+        collection.add(CB02_);
+        collection.add(CA91B);
+        Assert.assertThat(collection.size(), is(3));
+
+        assertThat(collection.removeAll(CA01B, ComplexStack.Match.EXACT), is(0));
+        Assert.assertThat(collection.size(), is(3));
+        assertThat(collection.removeAll(CA91B, ComplexStack.Match.EXACT), is(1));
+        Assert.assertThat(collection.size(), is(2));
+        assertThat(collection.removeAll(CB02_, ComplexStack.Match.EXACT), is(1));
+        Assert.assertThat(collection.size(), is(1));
+        assertThat(collection.removeAll(CA01_, ComplexStack.Match.EXACT), is(1));
+        Assert.assertThat(collection.size(), is(0));
+    }
+
+    @Test
+    public void testRemoveAllAny() {
+        collection.add(CA01_);
+        collection.add(CB02_);
+        collection.add(CA91B);
+        Assert.assertThat(collection.size(), is(3));
+
+        assertThat(collection.removeAll(CA01B, ComplexStack.Match.ANY), is(3));
+        Assert.assertThat(collection.size(), is(0));
+        assertThat(collection.removeAll(CA91B, ComplexStack.Match.ANY), is(0));
+        Assert.assertThat(collection.size(), is(0));
+        assertThat(collection.removeAll(CB02_, ComplexStack.Match.ANY), is(0));
+        Assert.assertThat(collection.size(), is(0));
+        assertThat(collection.removeAll(CA01_, ComplexStack.Match.ANY), is(0));
+        Assert.assertThat(collection.size(), is(0));
+    }
+
+    @Test
+    public void testRemoveAllIterable() {
+        collection.add(CA01_);
+        collection.add(CB02_);
+        collection.add(CA91B);
+        Assert.assertThat(collection.size(), is(3));
+
+        assertThat(collection.removeAll(Lists.newArrayList(CA01_, CB02_, CA91B, CA01B)), is(3));
+        Assert.assertThat(collection.size(), is(0));
+    }
+
+    @Test
+    public void testRemoveAllIterableMatchAny() {
+        collection.add(CA01_);
+        collection.add(CB02_);
+        collection.add(CA91B);
+        Assert.assertThat(collection.size(), is(3));
+
+        assertThat(collection.removeAll(Lists.newArrayList(CA01_, CB02_), ComplexStack.Match.ANY), is(3));
+        Assert.assertThat(collection.size(), is(0));
+    }
+
+    @Test
+    public void testRemoveAllIterableMatchExact() {
+        collection.add(CA01_);
+        collection.add(CB02_);
+        collection.add(CA91B);
+        Assert.assertThat(collection.size(), is(3));
+
+        assertThat(collection.removeAll(Lists.newArrayList(CA01_, CB02_), ComplexStack.Match.EXACT), is(2));
+        Assert.assertThat(collection.size(), is(1));
+    }
+
+}
