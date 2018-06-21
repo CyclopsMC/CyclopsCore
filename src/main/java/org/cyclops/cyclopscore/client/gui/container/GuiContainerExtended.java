3b6c7575546676a7afa1f51604b84771036d1523
Ruben Taelman <rubensworks@gmail.com>
2018-06-21 19:18:43 +0200

    Abstract tooltip rendering to GuiHelpers

--- src/main/java/org/cyclops/cyclopscore/client/gui/container/GuiContainerExtended.java
+++ src/main/java/org/cyclops/cyclopscore/client/gui/container/GuiContainerExtended.java
@@ -8,6 +8,7 @@ import net.minecraft.inventory.Slot;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 import org.cyclops.cyclopscore.CyclopsCore;
+import org.cyclops.cyclopscore.helper.GuiHelpers;
 import org.cyclops.cyclopscore.helper.RenderHelpers;
 import org.cyclops.cyclopscore.inventory.IValueNotifiable;
 import org.cyclops.cyclopscore.inventory.container.ExtendedInventoryContainer;
@@ -15,7 +16,6 @@ import org.cyclops.cyclopscore.inventory.container.button.IButtonActionClient;
 import org.cyclops.cyclopscore.inventory.container.button.IButtonClickAcceptorClient;
 import org.cyclops.cyclopscore.inventory.slot.SlotExtended;
 import org.cyclops.cyclopscore.network.packet.ButtonClickPacket;
-import org.lwjgl.opengl.GL11;
 
 import java.awt.*;
 import java.io.IOException;
@@ -104,78 +104,7 @@ public abstract class GuiContainerExtended extends GuiContainer implements
     }
 
     public void drawTooltip(List<String> lines, int x, int y) {
-        GlStateManager.pushMatrix();
-        GL11.glDisable(GL11.GL_DEPTH_TEST);
-        GlStateManager.disableRescaleNormal();
-        GlStateManager.disableLighting();
-        
-        int tooltipWidth = 0;
-        int tempWidth;
-        int xStart;
-        int yStart;
-        
-        for(String line : lines) {
-            tempWidth = this.fontRenderer.getStringWidth(line);
-            
-            if(tempWidth > tooltipWidth) {
-                tooltipWidth = tempWidth;
-            }
-        }
-        
-        xStart = x + 12;
-        yStart = y - 12;
-        int tooltipHeight = 8;
-        
-        if(lines.size() > 1) {
-            tooltipHeight += 2 + (lines.size() - 1) * 10;
-        }
-
-        if(this.guiLeft + xStart + tooltipWidth + 6 > this.width) {
-            xStart = this.width - tooltipWidth - this.guiLeft - 6;
-        }
-        
-        if(this.guiTop + yStart + tooltipHeight + 6 > this.height) {
-            yStart = this.height - tooltipHeight - this.guiTop - 6;
-        }
-        
-        this.zLevel = 300.0F;
-        itemRender.zLevel = 300.0F;
-        int color1 = -267386864;
-        this.drawGradientRect(xStart - 3, yStart - 4, xStart + tooltipWidth + 3, yStart - 3, color1, color1);
-        this.drawGradientRect(xStart - 3, yStart + tooltipHeight + 3, xStart + tooltipWidth + 3, yStart + tooltipHeight + 4, color1, color1);
-        this.drawGradientRect(xStart - 3, yStart - 3, xStart + tooltipWidth + 3, yStart + tooltipHeight + 3, color1, color1);
-        this.drawGradientRect(xStart - 4, yStart - 3, xStart - 3, yStart + tooltipHeight + 3, color1, color1);
-        this.drawGradientRect(xStart + tooltipWidth + 3, yStart - 3, xStart + tooltipWidth + 4, yStart + tooltipHeight + 3, color1, color1);
-        int color2 = 1347420415;
-        int color3 = (color2 & 16711422) >> 1 | color2 & -16777216;
-        this.drawGradientRect(xStart - 3, yStart - 3 + 1, xStart - 3 + 1, yStart + tooltipHeight + 3 - 1, color2, color3);
-        this.drawGradientRect(xStart + tooltipWidth + 2, yStart - 3 + 1, xStart + tooltipWidth + 3, yStart + tooltipHeight + 3 - 1, color2, color3);
-        this.drawGradientRect(xStart - 3, yStart - 3, xStart + tooltipWidth + 3, yStart - 3 + 1, color2, color2);
-        this.drawGradientRect(xStart - 3, yStart + tooltipHeight + 2, xStart + tooltipWidth + 3, yStart + tooltipHeight + 3, color3, color3);
-        
-        for(int stringIndex = 0; stringIndex < lines.size(); ++stringIndex) {
-            String line = lines.get(stringIndex);
-            
-            if(stringIndex == 0) {
-                line = "\u00a7" + Integer.toHexString(15) + line;
-            } else {
-                line = "\u00a77" + line;
-            }
-            
-            this.fontRenderer.drawStringWithShadow(line, xStart, yStart, -1);
-            
-            if(stringIndex == 0) {
-                yStart += 2;
-            }
-            
-            yStart += 10;
-        }
-        
-        GlStateManager.popMatrix();
-        GL11.glEnable(GL11.GL_DEPTH_TEST);
-        
-        this.zLevel = 0.0F;
-        itemRender.zLevel = 0.0F;
+        GuiHelpers.drawTooltip(this, lines, x, y);
     }
 
     @Override
@@ -233,14 +162,14 @@ public abstract class GuiContainerExtended extends GuiContainer implements
     /**
      * @return The total gui left offset.
      */
-    public int getGuiLeft() {
+    public int getGuiLeftTotal() {
         return this.guiLeft + offsetX;
     }
 
     /**
      * @return The total gui top offset.
      */
-    public int getGuiTop() {
+    public int getGuiTopTotal() {
         return this.guiTop + offsetY;
     }
 
--- src/main/java/org/cyclops/cyclopscore/helper/GuiHelpers.java
+++ src/main/java/org/cyclops/cyclopscore/helper/GuiHelpers.java
@@ -1,14 +1,19 @@
 package org.cyclops.cyclopscore.helper;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.FluidStack;
 import org.apache.commons.lang3.tuple.Triple;
-import org.cyclops.cyclopscore.client.gui.container.GuiContainerExtended;
+import org.lwjgl.opengl.GL11;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -19,6 +24,15 @@ import java.util.function.Supplier;
  * Helpers related to guis.
  */
 public class GuiHelpers {
+
+    /**
+     * The default item slot size. Width and height are equal.
+     */
+    public static int SLOT_SIZE = 18;
+    /**
+     * The default inner item slot size. Width and height are equal.
+     */
+    public static int SLOT_SIZE_INNER = 16;
 
     /**
      * Render a fluid tank in a gui.
@@ -135,6 +149,134 @@ public class GuiHelpers {
     }
 
     /**
+     * Draw a tooltip.
+     * @param gui The gui to draw in.
+     * @param lines A list of lines.
+     * @param x Tooltip X.
+     * @param y Tooltip Y.
+     */
+    public static void drawTooltip(GuiContainer gui, List<String> lines, int x, int y) {
+        int guiLeft = gui.getGuiLeft();
+        int guiTop = gui.getGuiTop();
+        int width = gui.width;
+        int height = gui.height;
+        Minecraft mc = Minecraft.getMinecraft();
+
+        GlStateManager.pushMatrix();
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
+        GlStateManager.disableRescaleNormal();
+        GlStateManager.disableLighting();
+
+        int tooltipWidth = 0;
+        int tempWidth;
+        int xStart;
+        int yStart;
+
+        for(String line : lines) {
+            tempWidth = mc.fontRenderer.getStringWidth(line);
+
+            if(tempWidth > tooltipWidth) {
+                tooltipWidth = tempWidth;
+            }
+        }
+
+        xStart = x + 12;
+        yStart = y - 12;
+        int tooltipHeight = 8;
+
+        if(lines.size() > 1) {
+            tooltipHeight += 2 + (lines.size() - 1) * 10;
+        }
+
+        if(guiLeft + xStart + tooltipWidth + 6 > width) {
+            xStart = width - tooltipWidth - guiLeft - 6;
+        }
+
+        if(guiTop + yStart + tooltipHeight + 6 > height) {
+            yStart = height - tooltipHeight - guiTop - 6;
+        }
+
+        float zLevel = 300.0F;
+        mc.getRenderItem().zLevel = 300.0F;
+        int color1 = -267386864;
+        drawGradientRect(xStart - 3, yStart - 4, xStart + tooltipWidth + 3, yStart - 3, color1, color1, zLevel);
+        drawGradientRect(xStart - 3, yStart + tooltipHeight + 3, xStart + tooltipWidth + 3, yStart + tooltipHeight + 4, color1, color1, zLevel);
+        drawGradientRect(xStart - 3, yStart - 3, xStart + tooltipWidth + 3, yStart + tooltipHeight + 3, color1, color1, zLevel);
+        drawGradientRect(xStart - 4, yStart - 3, xStart - 3, yStart + tooltipHeight + 3, color1, color1, zLevel);
+        drawGradientRect(xStart + tooltipWidth + 3, yStart - 3, xStart + tooltipWidth + 4, yStart + tooltipHeight + 3, color1, color1, zLevel);
+        int color2 = 1347420415;
+        int color3 = (color2 & 16711422) >> 1 | color2 & -16777216;
+        drawGradientRect(xStart - 3, yStart - 3 + 1, xStart - 3 + 1, yStart + tooltipHeight + 3 - 1, color2, color3, zLevel);
+        drawGradientRect(xStart + tooltipWidth + 2, yStart - 3 + 1, xStart + tooltipWidth + 3, yStart + tooltipHeight + 3 - 1, color2, color3, zLevel);
+        drawGradientRect(xStart - 3, yStart - 3, xStart + tooltipWidth + 3, yStart - 3 + 1, color2, color2, zLevel);
+        drawGradientRect(xStart - 3, yStart + tooltipHeight + 2, xStart + tooltipWidth + 3, yStart + tooltipHeight + 3, color3, color3, zLevel);
+
+        for(int stringIndex = 0; stringIndex < lines.size(); ++stringIndex) {
+            String line = lines.get(stringIndex);
+
+            if(stringIndex == 0) {
+                line = "\u00a7" + Integer.toHexString(15) + line;
+            } else {
+                line = "\u00a77" + line;
+            }
+
+            mc.fontRenderer.drawStringWithShadow(line, xStart, yStart, -1);
+
+            if(stringIndex == 0) {
+                yStart += 2;
+            }
+
+            yStart += 10;
+        }
+
+        GlStateManager.popMatrix();
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+
+        mc.getRenderItem().zLevel = 0.0F;
+    }
+
+    /**
+     * Render a rectangle.
+     *
+     * Static variant of {@link Gui#drawGradientRect(int, int, int, int, int, int)}.
+     *
+     * @param left Left X.
+     * @param top Top Y.
+     * @param right Right X.
+     * @param bottom Bottom Y.
+     * @param startColor Start gradient color.
+     * @param endColor End gradient color.
+     * @param zLevel The Z level to render at.
+     */
+    public static void drawGradientRect(int left, int top, int right, int bottom, int startColor, int endColor, float zLevel) {
+        float f = (float)(startColor >> 24 & 255) / 255.0F;
+        float f1 = (float)(startColor >> 16 & 255) / 255.0F;
+        float f2 = (float)(startColor >> 8 & 255) / 255.0F;
+        float f3 = (float)(startColor & 255) / 255.0F;
+        float f4 = (float)(endColor >> 24 & 255) / 255.0F;
+        float f5 = (float)(endColor >> 16 & 255) / 255.0F;
+        float f6 = (float)(endColor >> 8 & 255) / 255.0F;
+        float f7 = (float)(endColor & 255) / 255.0F;
+        GlStateManager.disableTexture2D();
+        GlStateManager.enableBlend();
+        GlStateManager.disableAlpha();
+        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        GlStateManager.shadeModel(7425);
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder bufferbuilder = tessellator.getBuffer();
+        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_COLOR);
+        bufferbuilder.pos((double)right, (double)top, (double)zLevel).color(f1, f2, f3, f).endVertex();
+        bufferbuilder.pos((double)left, (double)top, (double)zLevel).color(f1, f2, f3, f).endVertex();
+        bufferbuilder.pos((double)left, (double)bottom, (double)zLevel).color(f5, f6, f7, f4).endVertex();
+        bufferbuilder.pos((double)right, (double)bottom, (double)zLevel).color(f5, f6, f7, f4).endVertex();
+        tessellator.draw();
+        GlStateManager.shadeModel(7424);
+        GlStateManager.disableBlend();
+        GlStateManager.enableAlpha();
+        GlStateManager.enableTexture2D();
+    }
+
+    /**
      * Render a tooltip if the mouse if in the bounding box defined by the given position, width and height.
      * The tooltip lines supplier can return an optional list.
      * @param gui The gui to render in.
@@ -148,11 +290,11 @@ public class GuiHelpers {
      *                      No tooltip will be rendered when the optional value is absent.
      *                      This will only be called when needed.
      */
-    public static void renderTooltipOptional(GuiContainerExtended gui, int x, int y, int width, int height,
+    public static void renderTooltipOptional(GuiContainer gui, int x, int y, int width, int height,
                                              int mouseX, int mouseY, Supplier<Optional<List<String>>> linesSupplier) {
-        if(gui.isPointInRegion(x, y, width, height, mouseX, mouseY)) {
+        if(RenderHelpers.isPointInRegion(x, y, width, height, mouseX - gui.getGuiLeft(), mouseY - gui.getGuiTop())) {
             linesSupplier.get().ifPresent(
-                    lines -> gui.drawTooltip(lines, mouseX - gui.getGuiLeft(), mouseY - gui.getGuiTop()));
+                    lines -> drawTooltip(gui, lines, mouseX - gui.getGuiLeft(), mouseY - gui.getGuiTop()));
         }
     }
 
@@ -168,7 +310,7 @@ public class GuiHelpers {
      * @param linesSupplier A supplier for the tooltip lines to render.
      *                      This will only be called when needed.
      */
-    public static void renderTooltip(GuiContainerExtended gui, int x, int y, int width, int height,
+    public static void renderTooltip(GuiContainer gui, int x, int y, int width, int height,
                                      int mouseX, int mouseY, Supplier<List<String>> linesSupplier) {
         renderTooltipOptional(gui, x, y, width, height, mouseX, mouseY, () -> Optional.of(linesSupplier.get()));
     }
