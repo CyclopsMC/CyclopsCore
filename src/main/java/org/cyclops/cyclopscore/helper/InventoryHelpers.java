f6b9bcf6fc1f9b69f7f87be2f8b42f4502041784
Ruben Taelman <rubensworks@gmail.com>
2018-07-14 17:04:37 +0200

    Add hand-sensitive tryReAddToStack inventory helper method

--- src/main/java/org/cyclops/cyclopscore/helper/InventoryHelpers.java
+++ src/main/java/org/cyclops/cyclopscore/helper/InventoryHelpers.java
@@ -25,7 +25,7 @@ public class InventoryHelpers {
 	        inventory.setInventorySlotContents(i, ItemStack.EMPTY);
 	    }
 	}
-	
+
 	/**
 	 * Try adding a new item stack originating from the given original stack to the same original stack.
 	 * The original item stack should not have it's stack-size decreased yet, this method does this.
@@ -34,10 +34,24 @@ public class InventoryHelpers {
 	 * @param originalStack The original item stack from which the new item stack originated.
 	 * @param newStackPart The new item stack.
 	 */
+	@Deprecated
 	public static void tryReAddToStack(EntityPlayer player, ItemStack originalStack, ItemStack newStackPart) {
+		tryReAddToStack(player, originalStack, newStackPart, EnumHand.MAIN_HAND);
+	}
+	
+	/**
+	 * Try adding a new item stack originating from the given original stack to the same original stack.
+	 * The original item stack should not have it's stack-size decreased yet, this method does this.
+	 * Otherwise it will add the new stack to another inventory slot and in the worst case drop it on the floor.
+	 * @param player The player.
+	 * @param originalStack The original item stack from which the new item stack originated.
+	 * @param newStackPart The new item stack.
+	 * @param hand The hand for which the stack should be re-added
+	 */
+	public static void tryReAddToStack(EntityPlayer player, ItemStack originalStack, ItemStack newStackPart, EnumHand hand) {
 		if (!player.capabilities.isCreativeMode) {
         	if(!originalStack.isEmpty() && originalStack.getCount() == 1) {
-        		player.inventory.setInventorySlotContents(player.inventory.currentItem, newStackPart);
+        		player.inventory.setInventorySlotContents(hand == EnumHand.MAIN_HAND ? player.inventory.currentItem : 40, newStackPart);
         	} else {
                 if(!originalStack.isEmpty()) {
 					originalStack.shrink(1);
