9b2deeea5d510395462970cbb198ca086c9435f2
Ruben Taelman <rubensworks@gmail.com>
2018-08-15 17:28:36 +0200

    Add missing AdvancedButtonEnum class

---
+++ src/main/java/org/cyclops/cyclopscore/infobook/AdvancedButtonEnum.java
@@ -0,0 +1,13 @@
+package org.cyclops.cyclopscore.infobook;
+
+public class AdvancedButtonEnum {
+
+    private AdvancedButtonEnum() {
+
+    }
+
+    public static AdvancedButtonEnum create() {
+        return new AdvancedButtonEnum();
+    }
+
+}
