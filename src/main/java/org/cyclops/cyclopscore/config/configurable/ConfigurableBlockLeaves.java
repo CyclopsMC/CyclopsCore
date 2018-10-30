4799a815891d8130f533262ca74539fc0575bb29
Ruben Taelman <rubensworks@gmail.com>
2018-10-30 20:39:49 +0100

    Make leaves always drop the base item when silk-touched
    Closes CyclopsMC/IntegratedDynamics#633

--- src/main/java/org/cyclops/cyclopscore/config/configurable/ConfigurableBlockLeaves.java
+++ src/main/java/org/cyclops/cyclopscore/config/configurable/ConfigurableBlockLeaves.java
@@ -9,12 +9,14 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.color.IBlockColor;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -135,5 +137,15 @@ public abstract class ConfigurableBlockLeaves extends BlockLeaves implements ICo
     @Override
     public List<ItemStack> onSheared(ItemStack item, IBlockAccess world, BlockPos pos, int fortune) {
         return Lists.newArrayList(new ItemStack(this));
+    }
+
+    @Override
+    public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player) {
+        return new ItemStack(this);
+    }
+
+    @Override
+    protected ItemStack getSilkTouchDrop(IBlockState state) {
+        return new ItemStack(this);
     }
 }
