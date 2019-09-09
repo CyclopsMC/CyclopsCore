5d7aa13f9f82c7b35eedf7d0b5996b6d06c1e3e4
Ruben Taelman <rubensworks@gmail.com>
2019-09-09 20:52:09 +0200

    Fix blur particle not rendering

--- src/main/java/org/cyclops/cyclopscore/client/particle/ParticleBlur.java
+++ src/main/java/org/cyclops/cyclopscore/client/particle/ParticleBlur.java
@@ -1,5 +1,6 @@
 package org.cyclops.cyclopscore.client.particle;
 
+import com.google.common.collect.Queues;
 import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.IParticleRenderType;
@@ -7,29 +8,43 @@ import net.minecraft.client.particle.Particle;
 import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.texture.AtlasTexture;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import org.cyclops.cyclopscore.Reference;
 import org.cyclops.cyclopscore.helper.RenderHelpers;
 import org.lwjgl.opengl.GL11;
+
+import java.util.Queue;
 
 /**
  * A blurred static fading particle with any possible color.
  * @author rubensworks
  *
  */
+@Mod.EventBusSubscriber(Dist.CLIENT)
 public class ParticleBlur extends Particle {
-	
+
+	private static final Queue<ParticleBlur> BLURS_PENDING_RENDER = Queues.newLinkedBlockingDeque();
 	private static final ResourceLocation TEXTURE = new ResourceLocation(
 			Reference.MOD_ID, Reference.TEXTURE_PATH_PARTICLES + "particle_blur.png");
 	private static final int MAX_VIEW_DISTANCE = 30;
 	
-	private int scaleLife;
+	private float scaleLife;
 	private float originalScale;
+
+	// Last params from renderParticle
+	private float f;
+	private float f1;
+	private float f2;
+	private float f3;
+	private float f4;
+	private float f5;
 
 	public ParticleBlur(ParticleBlurData data, World world, double x, double y, double z,
 						double motionX, double motionY, double motionZ) {
@@ -41,9 +56,10 @@ public class ParticleBlur extends Particle {
 		this.particleRed = data.getRed();
 		this.particleGreen = data.getGreen();
 		this.particleBlue = data.getBlue();
+		this.particleAlpha = 0.9F;
 		this.particleGravity = 0;
 		
-		this.originalScale *= data.getScale();
+		this.originalScale = (this.rand.nextFloat() * 0.5F + 0.5F) * 2.0F * data.getScale();
 		this.maxAge = (int) ((rand.nextFloat() * 0.33F + 0.66F) * data.getAgeMultiplier());
 		this.setSize(0.01F, 0.01F);
 		
@@ -51,7 +67,7 @@ public class ParticleBlur extends Particle {
 		this.prevPosY = posY;
 		this.prevPosZ = posZ;
 		
-		this.scaleLife = (int) (maxAge / 2.5);
+		this.scaleLife = (float) (maxAge / 2.5);
 		
 		validateDistance();
 	}
@@ -65,7 +81,7 @@ public class ParticleBlur extends Particle {
 		}
 
 		if(renderentity == null
-				|| renderentity.getDistanceSq(posX, posY, posZ) > visibleDistance) {
+				|| renderentity.getDistanceSq(posX, posY, posZ) > visibleDistance * visibleDistance) {
 			maxAge = 0;
 		}
 	}
@@ -73,57 +89,18 @@ public class ParticleBlur extends Particle {
     @Override
     public void renderParticle(BufferBuilder worldRenderer, ActiveRenderInfo renderInfo,
 							   float f, float f1, float f2, float f3, float f4, float f5) {
-		float agescale = (float)age / (float) scaleLife;
-		if(agescale > 1F) {
-			agescale = 2 - agescale;
-		}
-
-		originalScale = originalScale * agescale;
-
-		int oldDrawMode = worldRenderer.getDrawMode();
-		VertexFormat oldVertexFormat = worldRenderer.getVertexFormat();
-        Tessellator.getInstance().draw();
-		GlStateManager.pushMatrix();
-
-		GlStateManager.depthMask(false);
-		GlStateManager.enableBlend();
-		GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
-
-		RenderHelpers.bindTexture(TEXTURE);
-
-		GlStateManager.color4f(1.0F, 1.0F, 1.0F, 0.75F);
-
-		float f10 = 0.5F * originalScale;
-		float f11 = (float)(prevPosX + (posX - prevPosX) * f - interpPosX);
-		float f12 = (float)(prevPosY + (posY - prevPosY) * f - interpPosY);
-		float f13 = (float)(prevPosZ + (posZ - prevPosZ) * f - interpPosZ);
-
-		worldRenderer.begin(GL11.GL_QUADS, DefaultVertexFormats.PARTICLE_POSITION_TEX_COLOR_LMAP);
-		int i = this.getBrightnessForRender(f5);
-		int j = i >> 16 & 65535;
-		int k = i & 65535;
-        worldRenderer.pos(f11 - f1 * f10 - f4 * f10, f12 - f2 * f10, f13 - f3 * f10 - f5 * f10).tex(0, 1).
-				color(particleRed, particleGreen, particleBlue, 0.9F).lightmap(j, k).endVertex();
-        worldRenderer.pos(f11 - f1 * f10 + f4 * f10, f12 + f2 * f10, f13 - f3 * f10 + f5 * f10).tex(1, 1).
-				color(particleRed, particleGreen, particleBlue, 0.9F).lightmap(j, k).endVertex();
-        worldRenderer.pos(f11 + f1 * f10 + f4 * f10, f12 + f2 * f10, f13 + f3 * f10 + f5 * f10).tex(1, 0).
-				color(particleRed, particleGreen, particleBlue, 0.9F).lightmap(j, k).endVertex();
-        worldRenderer.pos(f11 + f1 * f10 - f4 * f10, f12 - f2 * f10, f13 + f3 * f10 - f5 * f10).tex(0, 0).
-				color(particleRed, particleGreen, particleBlue, 0.9F).lightmap(j, k).endVertex();
-
-        Tessellator.getInstance().draw();
-
-		GlStateManager.disableBlend();
-		GlStateManager.depthMask(true);
-
-		GlStateManager.popMatrix();
-		RenderHelpers.bindTexture(AtlasTexture.LOCATION_PARTICLES_TEXTURE);
-		worldRenderer.begin(oldDrawMode, oldVertexFormat);
+		this.f = f;
+		this.f1 = f1;
+		this.f2 = f2;
+		this.f3 = f3;
+		this.f4 = f4;
+		this.f5 = f5;
+		BLURS_PENDING_RENDER.add(this);
 	}
 
 	@Override
 	public IParticleRenderType getRenderType() {
-		return IParticleRenderType.PARTICLE_SHEET_TRANSLUCENT;
+		return IParticleRenderType.CUSTOM;
 	}
 
 	@Override
@@ -151,6 +128,67 @@ public class ParticleBlur extends Particle {
 	 */
 	public void setGravity(float particleGravity) {
 		this.particleGravity = particleGravity;
+	}
+
+	private void renderQueued(BufferBuilder worldRenderer) {
+		float agescale = (float)age / (float) scaleLife;
+		if(agescale > 1F) {
+			agescale = 2 - agescale;
+		}
+
+		float currentScale = originalScale * agescale;
+
+		float f10 = 0.5F * currentScale;
+		float f11 = (float)(prevPosX + (posX - prevPosX) * f - interpPosX);
+		float f12 = (float)(prevPosY + (posY - prevPosY) * f - interpPosY);
+		float f13 = (float)(prevPosZ + (posZ - prevPosZ) * f - interpPosZ);
+
+		int i = this.getBrightnessForRender(f5);
+		int j = i >> 16 & 65535;
+		int k = i & 65535;
+		worldRenderer.pos(f11 - f1 * f10 - f4 * f10, f12 - f2 * f10, f13 - f3 * f10 - f5 * f10).tex(0, 1).
+				color(particleRed, particleGreen, particleBlue, 0.9F).lightmap(j, k).endVertex();
+		worldRenderer.pos(f11 - f1 * f10 + f4 * f10, f12 + f2 * f10, f13 - f3 * f10 + f5 * f10).tex(1, 1).
+				color(particleRed, particleGreen, particleBlue, 0.9F).lightmap(j, k).endVertex();
+		worldRenderer.pos(f11 + f1 * f10 + f4 * f10, f12 + f2 * f10, f13 + f3 * f10 + f5 * f10).tex(1, 0).
+				color(particleRed, particleGreen, particleBlue, 0.9F).lightmap(j, k).endVertex();
+		worldRenderer.pos(f11 + f1 * f10 - f4 * f10, f12 - f2 * f10, f13 + f3 * f10 - f5 * f10).tex(0, 0).
+				color(particleRed, particleGreen, particleBlue, 0.9F).lightmap(j, k).endVertex();
+	}
+
+	@SubscribeEvent
+	public static void onRenderWorldLast(RenderWorldLastEvent event) {
+		if (!BLURS_PENDING_RENDER.isEmpty()) {
+			GlStateManager.pushMatrix();
+
+			GL11.glPushAttrib(GL11.GL_LIGHTING_BIT);
+			GlStateManager.depthMask(false);
+			GlStateManager.enableBlend();
+			GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
+			GlStateManager.disableLighting();
+
+			RenderHelpers.bindTexture(TEXTURE);
+
+			GlStateManager.color4f(1.0F, 1.0F, 1.0F, 0.75F);
+
+			BufferBuilder worldRenderer = Tessellator.getInstance().getBuffer();
+			worldRenderer.begin(GL11.GL_QUADS, DefaultVertexFormats.PARTICLE_POSITION_TEX_COLOR_LMAP);
+
+			ParticleBlur particleBlur;
+			while ((particleBlur = BLURS_PENDING_RENDER.poll()) != null) {
+				particleBlur.renderQueued(worldRenderer);
+			}
+
+			Tessellator.getInstance().draw();
+
+			GlStateManager.disableBlend();
+			GlStateManager.depthMask(true);
+			GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+
+			GlStateManager.popMatrix();
+			GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1F);
+			GL11.glPopAttrib();
+		}
 	}
 
 }
