f2ccfcd856efc0abc7aef1b9bf3d966c401c4fbb
Ruben Taelman <rubensworks@gmail.com>
2019-11-21 19:44:39 +0100

    Add cached recipe lookup helper method

--- src/main/java/org/cyclops/cyclopscore/helper/CraftingHelpers.java
+++ src/main/java/org/cyclops/cyclopscore/helper/CraftingHelpers.java
@@ -1,21 +1,42 @@
 package org.cyclops.cyclopscore.helper;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.oredict.OreDictionary;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Several convenience functions for crafting.
  * @author rubensworks
  */
 public class CraftingHelpers {
+
+    private static final LoadingCache<Pair<CacheableInventoryCrafting, Integer>, Optional<IRecipe>> CACHE_RECIPES = CacheBuilder.newBuilder()
+            .expireAfterWrite(1, TimeUnit.MINUTES)
+            .build(new CacheLoader<Pair<CacheableInventoryCrafting, Integer>, Optional<IRecipe>>() {
+                @Override
+                public Optional<IRecipe> load(Pair<CacheableInventoryCrafting, Integer> key) throws Exception {
+                    IRecipe recipe = CraftingManager.findMatchingRecipe(key.getLeft().getInventoryCrafting(), DimensionManager.getWorld(key.getRight()));
+                    return Optional.ofNullable(recipe);
+                }
+            });
 
     public static IRecipe findCraftingRecipe(ItemStack itemStack, int index) throws IllegalArgumentException {
         int indexAttempt = index;
@@ -26,6 +47,19 @@ public class CraftingHelpers {
         }
         throw new IllegalArgumentException("Could not find crafting recipe for " + itemStack + "::"
                 + itemStack.getTagCompound() + " with index " + index);
+    }
+
+    /**
+     * A cache-based variant of {@link CraftingManager#findMatchingRecipe(InventoryCrafting, World)}.
+     * @param inventoryCrafting The crafting inventory.
+     * @param world The world.
+     * @param uniqueInventory If inventoryCrafting is a unique instance that can be cached safely.
+     *                        Otherwise a deep copy will be taken.
+     * @return The recipe or null.
+     */
+    public static IRecipe findMatchingRecipeCached(InventoryCrafting inventoryCrafting, World world, boolean uniqueInventory) {
+        return CACHE_RECIPES.getUnchecked(Pair.of(new CacheableInventoryCrafting(inventoryCrafting, !uniqueInventory), world.provider.getDimension()))
+                .orElse(null);
     }
 
     public static Map.Entry<ItemStack, ItemStack> findFurnaceRecipe(ItemStack itemStack, int index) throws IllegalArgumentException {
@@ -70,6 +104,57 @@ public class CraftingHelpers {
                        itemStack1.getItemDamage() == OreDictionary.WILDCARD_VALUE ||
                        itemStack2.getItemDamage() == OreDictionary.WILDCARD_VALUE ||
                        itemStack1.getItem().isDamageable());
+    }
+
+    public static class CacheableInventoryCrafting {
+
+        private final InventoryCrafting inventoryCrafting;
+
+        public CacheableInventoryCrafting(InventoryCrafting inventoryCrafting, boolean copyInventory) {
+            if (copyInventory) {
+                // Deep-copy of the inventory to enable caching
+                this.inventoryCrafting = new InventoryCrafting(new Container() {
+                    @Override
+                    public boolean canInteractWith(EntityPlayer playerIn) {
+                        return false;
+                    }
+                }, inventoryCrafting.getWidth(), inventoryCrafting.getHeight());
+                for (int i = 0; i < inventoryCrafting.getSizeInventory(); i++) {
+                    this.inventoryCrafting.setInventorySlotContents(i, inventoryCrafting.getStackInSlot(i).copy());
+                }
+            } else {
+                this.inventoryCrafting = inventoryCrafting;
+            }
+        }
+
+        public InventoryCrafting getInventoryCrafting() {
+            return inventoryCrafting;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof CacheableInventoryCrafting)) {
+                return false;
+            }
+            for (int i = 0; i < getInventoryCrafting().getSizeInventory(); i++) {
+                if (!ItemStack.areItemStacksEqual(getInventoryCrafting().getStackInSlot(i),
+                        ((CacheableInventoryCrafting) obj).getInventoryCrafting().getStackInSlot(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 11 + getInventoryCrafting().getSizeInventory();
+            for (int i = 0; i < getInventoryCrafting().getSizeInventory(); i++) {
+                hash = hash << 1;
+                hash |= ItemStackHelpers.getItemStackHashCode(getInventoryCrafting().getStackInSlot(i));
+            }
+            return hash;
+        }
+
     }
 
 }
