974951cd2f54ceede7e12dc906273bc9299d002e
Ruben Taelman <rubensworks@gmail.com>
2019-12-17 15:55:07 +0100

    Add NBT-based value notifier helper

--- src/main/java/org/cyclops/cyclopscore/helper/ValueNotifierHelpers.java
+++ src/main/java/org/cyclops/cyclopscore/helper/ValueNotifierHelpers.java
@@ -2,6 +2,7 @@ package org.cyclops.cyclopscore.helper;
 
 import com.google.common.collect.Lists;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.INBT;
 import net.minecraft.nbt.ListNBT;
 import net.minecraft.nbt.StringNBT;
 import net.minecraft.util.text.ITextComponent;
@@ -19,6 +20,18 @@ import java.util.List;
 public class ValueNotifierHelpers {
 
     public static String KEY = "v";
+
+    /**
+     * Set the NBT value
+     * @param notifier The notifier instance
+     * @param valueId The value id
+     * @param value The value
+     */
+    public static void setValue(IValueNotifier notifier, int valueId, INBT value) {
+        CompoundNBT tag = new CompoundNBT();
+        tag.put(KEY, value);
+        notifier.setValue(valueId, tag);
+    }
 
     /**
      * Set the int value
@@ -70,6 +83,21 @@ public class ValueNotifierHelpers {
         }
         tag.put(KEY, list);
         notifier.setValue(valueId, tag);
+    }
+
+    /**
+     * get the NBT value
+     * @param notifier The notifier instance
+     * @param valueId The value id
+     * @return The value
+     */
+    @Nullable
+    public static INBT getValueNbt(IValueNotifier notifier, int valueId) {
+        CompoundNBT tag = notifier.getValue(valueId);
+        if(tag != null) {
+            return tag.get(KEY);
+        }
+        return null;
     }
 
     /**
