8dd9e7c774e78f19d0ffb49642da125d8ec8cb41
Ruben Taelman <rubensworks@gmail.com>
2020-04-14 17:59:07 +0200

    Add supplier-based ItemStackTileEntityRendererBase
    Required for CyclopsMC/ColossalChests#131

---
+++ src/main/java/org/cyclops/cyclopscore/client/render/tileentity/ItemStackTileEntityRendererBase.java
@@ -0,0 +1,35 @@
+package org.cyclops.cyclopscore.client.render.tileentity;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.tileentity.ItemStackTileEntityRenderer;
+import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+
+import javax.annotation.Nullable;
+import java.util.function.Supplier;
+
+/**
+ * A supplier-based {@link ItemStackTileEntityRenderer} that caches the internal tile entity.
+ * Don't use this if the tile entity should be recreated at every render-tick.
+ * @author rubensworks
+ */
+public class ItemStackTileEntityRendererBase extends ItemStackTileEntityRenderer {
+
+    private final Supplier<TileEntity> tileEntitySupplier;
+    @Nullable
+    private TileEntity tileEntity;
+
+    public ItemStackTileEntityRendererBase(Supplier<TileEntity> tileEntitySupplier) {
+        this.tileEntitySupplier = tileEntitySupplier;
+    }
+
+    @Override
+    public void render(ItemStack itemStackIn, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int combinedLightIn, int combinedOverlayIn) {
+        if (this.tileEntity == null) {
+            this.tileEntity = this.tileEntitySupplier.get();
+        }
+        TileEntityRendererDispatcher.instance.renderItem(this.tileEntity, matrixStackIn, bufferIn, combinedLightIn, combinedOverlayIn);
+    }
+}
