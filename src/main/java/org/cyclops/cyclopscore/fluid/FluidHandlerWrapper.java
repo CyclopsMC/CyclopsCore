2aa45328ec03c8a514d39e720b06b0187068e6ee
Ruben Taelman <rubensworks@gmail.com>
2020-11-18 16:57:01 +0100

    Add FluidHandlerWrapper

---
+++ src/main/java/org/cyclops/cyclopscore/fluid/FluidHandlerWrapper.java
@@ -0,0 +1,56 @@
+package org.cyclops.cyclopscore.fluid;
+
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author rubensworks
+ */
+public class FluidHandlerWrapper implements IFluidHandler {
+
+    private final IFluidHandler fluidHandler;
+
+    public FluidHandlerWrapper(IFluidHandler fluidHandler) {
+        this.fluidHandler = fluidHandler;
+    }
+
+    @Override
+    public int getTanks() {
+        return this.fluidHandler.getTanks();
+    }
+
+    @Nonnull
+    @Override
+    public FluidStack getFluidInTank(int tank) {
+        return this.fluidHandler.getFluidInTank(tank);
+    }
+
+    @Override
+    public int getTankCapacity(int tank) {
+        return this.fluidHandler.getTankCapacity(tank);
+    }
+
+    @Override
+    public boolean isFluidValid(int tank, @Nonnull FluidStack stack) {
+        return this.fluidHandler.isFluidValid(tank, stack);
+    }
+
+    @Override
+    public int fill(FluidStack resource, FluidAction action) {
+        return this.fluidHandler.fill(resource, action);
+    }
+
+    @Nonnull
+    @Override
+    public FluidStack drain(FluidStack resource, FluidAction action) {
+        return this.fluidHandler.drain(resource, action);
+    }
+
+    @Nonnull
+    @Override
+    public FluidStack drain(int maxDrain, FluidAction action) {
+        return this.fluidHandler.drain(maxDrain, action);
+    }
+}
