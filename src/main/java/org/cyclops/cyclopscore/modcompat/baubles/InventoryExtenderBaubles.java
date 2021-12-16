a296a17e53eaac70352c172bc42279ce1e743188
Ruben Taelman <rubensworks@gmail.com>
2021-12-16 15:47:04 +0100

    Restore Baubles integration

--- src/main/java/org/cyclops/cyclopscore/modcompat/baubles/InventoryExtenderBaubles.java
+++ src/main/java/org/cyclops/cyclopscore/modcompat/baubles/InventoryExtenderBaubles.java
@@ -1,6 +1,6 @@
 package org.cyclops.cyclopscore.modcompat.baubles;
 
-
+import lazy.baubles.api.BaublesAPI;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.items.IItemHandlerModifiable;
 import org.cyclops.cyclopscore.inventory.PlayerExtendedInventoryIterator;
@@ -15,7 +15,6 @@ public class InventoryExtenderBaubles implements PlayerExtendedInventoryIterator
     @Override
     @Nullable
     public IItemHandlerModifiable getInventory(Player player) {
-        return null; // TODO
-        //return BaublesAPI.getBaublesHandler(player).orElse(null);
+        return BaublesAPI.getBaublesHandler(player).orElse(null);
     }
 }
