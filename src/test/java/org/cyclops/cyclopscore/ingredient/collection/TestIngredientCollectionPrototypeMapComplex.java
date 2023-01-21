CyclopsMC/CyclopsCore

commit 61267920638e48331a513b9a446907910122078d

Optimize containment checks in IngredientCollectionPrototypeMap
This fixes performance issues such as CyclopsMC/IntegratedTerminals#94

src/main/java/org/cyclops/cyclopscore/ingredient/collection/IngredientCollectionPrototypeMap.java
@@ -81,6 +81,32 @@ public class IngredientCollectionPrototypeMap<T, M> extends IngredientCollection
     }
 
     @Override
+    public boolean contains(T instance) {
+        IIngredientMatcher<T, M> matcher = getComponent().getMatcher();
+        T prototype = getPrototype(instance);
+        Long value = ingredients.get(prototype);
+        long existingValue = value == null ? 0 : value;
+        long currentValue = matcher.getQuantity(instance);
+        return currentValue == existingValue;
+    }
+
+    @Override
+    public boolean contains(T instance, M matchCondition) {
+        IIngredientMatcher<T, M> matcher = getComponent().getMatcher();
+        T prototype = getPrototype(instance);
+        Iterator<Map.Entry<T, Long>> it = ingredients.iterator(prototype, matchCondition);
+        while (it.hasNext()) {
+            Long value = it.next().getValue();
+            long existingValue = value == null ? 0 : value;
+            long currentValue = matcher.getQuantity(instance);
+            if (currentValue == existingValue) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
     public void clear() {
         ingredients.clear();
     }

src/test/java/org/cyclops/cyclopscore/ingredient/collection/TestIngredientCollectionPrototypeMapComplex.java
@@ -19,11 +19,15 @@ public class TestIngredientCollectionPrototypeMapComplex {
     private static final ComplexStack CA01_ = new ComplexStack(ComplexStack.Group.A, 0, 1, null);
     private static final ComplexStack CA0M1_ = new ComplexStack(ComplexStack.Group.A, 0, -1, null);
     private static final ComplexStack CA02_ = new ComplexStack(ComplexStack.Group.A, 0, 2, null);
+    private static final ComplexStack CA03_ = new ComplexStack(ComplexStack.Group.A, 0, 3, null);
+    private static final ComplexStack CA04_ = new ComplexStack(ComplexStack.Group.A, 0, 4, null);
     private static final ComplexStack CA05_ = new ComplexStack(ComplexStack.Group.A, 0, 5, null);
     private static final ComplexStack CA0M5_ = new ComplexStack(ComplexStack.Group.A, 0, -5, null);
 
     private static final ComplexStack CB01_ = new ComplexStack(ComplexStack.Group.B, 0, 1, null);
     private static final ComplexStack CB02_ = new ComplexStack(ComplexStack.Group.B, 0, 2, null);
+    private static final ComplexStack CB03_ = new ComplexStack(ComplexStack.Group.B, 0, 3, null);
+    private static final ComplexStack CB04_ = new ComplexStack(ComplexStack.Group.B, 0, 4, null);
     private static final ComplexStack CB05_ = new ComplexStack(ComplexStack.Group.B, 0, 5, null);
 
     private static final ComplexStack CA91B = new ComplexStack(ComplexStack.Group.A, 9, 1, ComplexStack.Tag.B);
@@ -126,6 +130,35 @@ public class TestIngredientCollectionPrototypeMapComplex {
         assertThat(collection.add(CA01_), is(true));
 
         assertThat(collection.isEmpty(), is(true));
+    }
+
+    @Test
+    public void contains() {
+        assertThat(collection.add(CA01_), is(true));
+        assertThat(collection.add(CA01_), is(true));
+        assertThat(collection.add(CA01_), is(true));
+
+        assertThat(collection.contains(CA01_), is(false));
+        assertThat(collection.contains(CA02_), is(false));
+        assertThat(collection.contains(CA03_), is(true));
+        assertThat(collection.contains(CA04_), is(false));
+    }
+
+    @Test
+    public void containsMatch() {
+        assertThat(collection.add(CA01_), is(true));
+        assertThat(collection.add(CA01_), is(true));
+        assertThat(collection.add(CA01_), is(true));
+
+        assertThat(collection.contains(CB01_, ComplexStack.Match.META), is(false));
+        assertThat(collection.contains(CB02_, ComplexStack.Match.META), is(false));
+        assertThat(collection.contains(CB03_, ComplexStack.Match.META), is(true));
+        assertThat(collection.contains(CB04_, ComplexStack.Match.META), is(false));
+
+        assertThat(collection.contains(CB01_, ComplexStack.Match.GROUP), is(false));
+        assertThat(collection.contains(CB02_, ComplexStack.Match.GROUP), is(false));
+        assertThat(collection.contains(CB03_, ComplexStack.Match.GROUP), is(false));
+        assertThat(collection.contains(CB04_, ComplexStack.Match.GROUP), is(false));
     }
 
     @Test

commit a52be3b7dc569c32ff249deb61b600ec2c22e0c0

Fix incorrect containment check in IngredientCollectionPrototypeMap
This is a regression since 61267920638e48331a513b9a446907910122078d

Closes CyclopsMC/IntegratedTerminals#98

src/main/java/org/cyclops/cyclopscore/ingredient/collection/IngredientCollectionPrototypeMap.java
@@ -96,10 +96,11 @@ public class IngredientCollectionPrototypeMap<T, M> extends IngredientCollection
         T prototype = getPrototype(instance);
         Iterator<Map.Entry<T, Long>> it = ingredients.iterator(prototype, matchCondition);
         while (it.hasNext()) {
+            boolean exactMatch = getComponent().getPrimaryQuantifier() != null && matcher.hasCondition(matchCondition, getComponent().getPrimaryQuantifier().getMatchCondition());
             Long value = it.next().getValue();
             long existingValue = value == null ? 0 : value;
             long currentValue = matcher.getQuantity(instance);
-            if (currentValue == existingValue) {
+            if (exactMatch ? currentValue == existingValue : currentValue <= existingValue) {
                 return true;
             }
         }

src/test/java/org/cyclops/cyclopscore/ingredient/collection/TestIngredientCollectionPrototypeMapComplex.java
@@ -150,8 +150,8 @@ public class TestIngredientCollectionPrototypeMapComplex {
         assertThat(collection.add(CA01_), is(true));
         assertThat(collection.add(CA01_), is(true));
 
-        assertThat(collection.contains(CB01_, ComplexStack.Match.META), is(false));
-        assertThat(collection.contains(CB02_, ComplexStack.Match.META), is(false));
+        assertThat(collection.contains(CB01_, ComplexStack.Match.META), is(true));
+        assertThat(collection.contains(CB02_, ComplexStack.Match.META), is(true));
         assertThat(collection.contains(CB03_, ComplexStack.Match.META), is(true));
         assertThat(collection.contains(CB04_, ComplexStack.Match.META), is(false));
 
@@ -159,6 +159,16 @@ public class TestIngredientCollectionPrototypeMapComplex {
         assertThat(collection.contains(CB02_, ComplexStack.Match.GROUP), is(false));
         assertThat(collection.contains(CB03_, ComplexStack.Match.GROUP), is(false));
         assertThat(collection.contains(CB04_, ComplexStack.Match.GROUP), is(false));
+
+        assertThat(collection.contains(CB01_, ComplexStack.Match.META | ComplexStack.Match.AMOUNT), is(false));
+        assertThat(collection.contains(CB02_, ComplexStack.Match.META | ComplexStack.Match.AMOUNT), is(false));
+        assertThat(collection.contains(CB03_, ComplexStack.Match.META | ComplexStack.Match.AMOUNT), is(true));
+        assertThat(collection.contains(CB04_, ComplexStack.Match.META | ComplexStack.Match.AMOUNT), is(false));
+
+        assertThat(collection.contains(CB01_, ComplexStack.Match.GROUP | ComplexStack.Match.AMOUNT), is(false));
+        assertThat(collection.contains(CB02_, ComplexStack.Match.GROUP | ComplexStack.Match.AMOUNT), is(false));
+        assertThat(collection.contains(CB03_, ComplexStack.Match.GROUP | ComplexStack.Match.AMOUNT), is(false));
+        assertThat(collection.contains(CB04_, ComplexStack.Match.GROUP | ComplexStack.Match.AMOUNT), is(false));
     }
 
     @Test