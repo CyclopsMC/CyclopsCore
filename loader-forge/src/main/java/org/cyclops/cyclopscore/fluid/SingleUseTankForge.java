3cb7e595acde59b45cba6d82ce31367c622e003f
Ruben Taelman <rubensworks@gmail.com>
2024-08-08 20:32:37 +0200

    Add fluid utilities for Forge

---
+++ loader-forge/src/main/java/org/cyclops/cyclopscore/fluid/SingleUseTankForge.java
@@ -0,0 +1,148 @@
+package org.cyclops.cyclopscore.fluid;
+
+import com.google.common.collect.Lists;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraftforge.fluids.FluidStack;
+import org.cyclops.cyclopscore.persist.IDirtyMarkListener;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * A simple tank that can accept and drain fluids until the capacity is reached.
+ * Only one fluid can be accepted, which must be specified with {@link SingleUseTankForge#setAcceptedFluid(Fluid)}.
+ * Based on the Buildcraft SingleUseTank.
+ *
+ * @author rubensworks
+ *
+ */
+public class SingleUseTankForge extends TankForge {
+
+    /**
+     * The NBT name for the fluid tank.
+     */
+    public static final String NBT_ACCEPTED_FLUID = "acceptedFluid";
+
+    private final List<IDirtyMarkListener> dirtyMarkListeners = Lists.newLinkedList();
+
+    private Fluid acceptedFluid;
+
+    /**
+     * Make a new tank instance.
+     * @param capacity The capacity (mB) for the tank.
+     */
+    public SingleUseTankForge(int capacity) {
+        super(capacity);
+        setAcceptedFluid(Fluids.EMPTY);
+    }
+
+    /**
+     * Add a dirty marking listener.
+     * @param dirtyMarkListener The dirty mark listener.
+     */
+    public synchronized void addDirtyMarkListener(IDirtyMarkListener dirtyMarkListener) {
+        this.dirtyMarkListeners.add(dirtyMarkListener);
+    }
+
+    /**
+     * Remove a dirty marking listener.
+     * @param dirtyMarkListener The dirty mark listener.
+     */
+    public synchronized void removeDirtyMarkListener(IDirtyMarkListener dirtyMarkListener) {
+        this.dirtyMarkListeners.remove(dirtyMarkListener);
+    }
+
+    @Override
+    public int fill(FluidStack resource, FluidAction action) {
+        Fluid acceptedFluid = getAcceptedFluid();
+        int filled = 0;
+        if (resource.isEmpty()) {
+            filled = 0;
+        } else {
+            if (action.execute() && acceptedFluid == Fluids.EMPTY) {
+                acceptedFluid = resource.getFluid();
+            }
+            if (acceptedFluid == Fluids.EMPTY || acceptedFluid == resource.getFluid()) {
+                filled = super.fill(resource, action);
+            }
+        }
+        if(action.execute() && filled > 0) {
+            sendUpdate();
+        }
+        return filled;
+    }
+
+    @Override
+    public FluidStack drain(int maxDrain, FluidAction action) {
+        FluidStack drained = super.drain(maxDrain, action);
+        if (action.execute() && !drained.isEmpty()) {
+            sendUpdate();
+        }
+        return drained;
+    }
+
+    @Override
+    public FluidStack drain(FluidStack resource, FluidAction action) {
+        FluidStack drained = super.drain(resource, action);
+        if (action.execute() && !drained.isEmpty()) {
+            sendUpdate();
+        }
+        return drained;
+    }
+
+    protected void sendUpdate() {
+        List<IDirtyMarkListener> dirtyMarkListeners;
+        synchronized (this) {
+            dirtyMarkListeners = Lists.newLinkedList(this.dirtyMarkListeners);
+        }
+        for(IDirtyMarkListener dirtyMarkListener : dirtyMarkListeners) {
+            dirtyMarkListener.onDirty();
+        }
+    }
+
+    /**
+     * Reset the tank by setting the inner fluid to null.
+     */
+    public void reset() {
+        acceptedFluid = Fluids.EMPTY;
+        setValidator(fluidStack -> true);
+    }
+
+    /**
+     * Set the accepted fluid for this tank.
+     * @param fluid The accepted fluid
+     */
+    public void setAcceptedFluid(Fluid fluid) {
+        this.acceptedFluid = Objects.requireNonNull(fluid);
+        if (fluid == Fluids.EMPTY) {
+            setValidator(fluidStack -> true);
+        } else {
+            setValidator(fluidStack -> fluidStack.getFluid() == fluid);
+        }
+    }
+
+    /**
+     * Get the accepted fluid for this tank.
+     * @return The accepted fluid.
+     */
+    public Fluid getAcceptedFluid() {
+        return acceptedFluid;
+    }
+
+    @Override
+    public void writeTankToNBT(CompoundTag nbt) {
+        super.writeTankToNBT(nbt);
+        nbt.putString(NBT_ACCEPTED_FLUID, BuiltInRegistries.FLUID.getKey(acceptedFluid).toString());
+    }
+
+    @Override
+    public void readTankFromNBT(CompoundTag nbt) {
+        super.readTankFromNBT(nbt);
+        setAcceptedFluid(BuiltInRegistries.FLUID.get(ResourceLocation.parse(nbt.getString(NBT_ACCEPTED_FLUID))));
+    }
+
+}
---
+++ loader-forge/src/main/java/org/cyclops/cyclopscore/fluid/TankForge.java
@@ -0,0 +1,109 @@
+package org.cyclops.cyclopscore.fluid;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.fluids.capability.templates.FluidTank;
+
+/**
+ * A simple fluid tank.
+ * Based on the Buildcraft Tank
+ * @author rubensworks
+ *
+ */
+public class TankForge extends FluidTank {
+
+    /**
+     * Make a new fluid tank.
+     * @param capacity The capacity (mB) for the tank.
+     */
+    public TankForge(int capacity) {
+        super(capacity);
+    }
+
+    /**
+     * Check if this tank is empty.
+     * @return If the tank is empty; no fluid is inside of it.
+     */
+    public boolean isEmpty() {
+        return getFluid().isEmpty() || getFluid().getAmount() <= 0;
+    }
+
+    /**
+     * Check if this tank is full; the capacity is reached.
+     * @return If this tank is full.
+     */
+    public boolean isFull() {
+        return !getFluid().isEmpty() && getFluid().getAmount() >= getCapacity();
+    }
+
+    /**
+     * Get the fluid that currently occupies this tank, will return null if there is no fluid.
+     * @return The inner fluid.
+     */
+    public Fluid getFluidType() {
+        return !getFluid().isEmpty() ? getFluid().getFluid() : null;
+    }
+
+    protected boolean replaceInnerFluid() {
+        return true;
+    }
+
+    @Override
+    public CompoundTag writeToNBT(CompoundTag nbt) {
+        if(replaceInnerFluid()) {
+            super.writeToNBT(nbt);
+        }
+        writeTankToNBT(nbt);
+        return nbt;
+    }
+
+    public CompoundTag writeToNBT(CompoundTag nbt, String tag) {
+        CompoundTag subTag = new CompoundTag();
+        writeToNBT(subTag);
+        nbt.put(tag, subTag);
+        return nbt;
+    }
+
+    @Override
+    public FluidTank readFromNBT(CompoundTag nbt) {
+        if(replaceInnerFluid()) {
+            if (nbt.contains("Empty")) {
+                setFluid(null);
+            }
+            super.readFromNBT(nbt);
+        }
+        readTankFromNBT(nbt);
+        return this;
+    }
+
+    public FluidTank readFromNBT(CompoundTag data, String tag) {
+        CompoundTag subTag = data.getCompound(tag);
+        return readFromNBT(subTag);
+    }
+
+    /**
+     * Write the tank contents to NBT.
+     * @param nbt The NBT tag to write to.
+     */
+    public void writeTankToNBT(CompoundTag nbt) {
+        nbt.putInt("capacity", getCapacity());
+    }
+
+    /**
+     * Read the tank contents from NBT.
+     * @param nbt The NBT tag to write from.
+     */
+    public void readTankFromNBT(CompoundTag nbt) {
+        if(nbt.contains("capacity")) { // Backwards compatibility.
+            setCapacity(nbt.getInt("capacity"));
+        }
+    }
+
+    @Override
+    public int getTankCapacity(int tank) {
+        if (tank == 0) {
+            return getCapacity();
+        }
+        return 0;
+    }
+}
