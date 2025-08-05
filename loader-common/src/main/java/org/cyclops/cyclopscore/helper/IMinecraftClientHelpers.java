0414fef0e9b9edc2c7c4c4628172cfef75c318b9
Ruben Taelman <rubensworks@gmail.com>
2025-08-05 13:58:06 +0200

    Add getPlayer to IMinecraftClientHelpers

--- loader-common/src/main/java/org/cyclops/cyclopscore/helper/IMinecraftClientHelpers.java
+++ loader-common/src/main/java/org/cyclops/cyclopscore/helper/IMinecraftClientHelpers.java
@@ -1,9 +1,16 @@
 package org.cyclops.cyclopscore.helper;
 
+import net.minecraft.world.entity.player.Player;
+
 /**
  * @author rubensworks
  */
 public interface IMinecraftClientHelpers {
+
+    /**
+     * @return The player instance.
+     */
+    public Player getPlayer();
 
     /**
      * @return If the user is shifted.
--- loader-common/src/main/java/org/cyclops/cyclopscore/helper/MinecraftClientHelpersCommon.java
+++ loader-common/src/main/java/org/cyclops/cyclopscore/helper/MinecraftClientHelpersCommon.java
@@ -1,11 +1,18 @@
 package org.cyclops.cyclopscore.helper;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.world.entity.player.Player;
 
 /**
  * @author rubensworks
  */
 public class MinecraftClientHelpersCommon implements IMinecraftClientHelpers {
+    @Override
+    public Player getPlayer() {
+        return Minecraft.getInstance().player;
+    }
+
     @Override
     public boolean isShifted() {
         return Screen.hasShiftDown();
