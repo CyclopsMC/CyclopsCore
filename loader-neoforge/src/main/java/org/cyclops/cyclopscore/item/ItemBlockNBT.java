617c25d1d6070d96edf718561e93efc15daeba35
Ruben Taelman <rubensworks@gmail.com>
2025-12-26 06:50:31 +0100

    Fix block entity data from item not being added to blocks
    Related to CyclopsMC/EvilCraft#1147

--- loader-neoforge/src/main/java/org/cyclops/cyclopscore/item/ItemBlockNBT.java
+++ loader-neoforge/src/main/java/org/cyclops/cyclopscore/item/ItemBlockNBT.java
@@ -40,9 +40,6 @@ public class ItemBlockNBT extends BlockItem {
 
         BlockEntity tile = world.getBlockEntity(pos);
         if (tile != null) {
-            if (!world.isClientSide() && (player == null || !player.canUseGameMasterBlocks())) {
-                return false;
-            }
             return itemStackDataToTile(itemStack.copy().split(1), tile);
         }
 
