e6a2cdfb59a63060c0b6cbeb5946d2b54726319e
Ruben Taelman <rubensworks@gmail.com>
2026-04-08 12:56:25 +0200

    Migrate loot modifier to ItemStackTemplate

--- loader-neoforge/src/main/java/org/cyclops/cyclopscore/loot/modifier/LootModifierInjectItem.java
+++ loader-neoforge/src/main/java/org/cyclops/cyclopscore/loot/modifier/LootModifierInjectItem.java
@@ -6,6 +6,7 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemStackTemplate;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.neoforged.neoforge.common.loot.IGlobalLootModifier;
@@ -21,24 +22,24 @@ import java.util.function.Supplier;
 public class LootModifierInjectItem extends LootModifier {
     public static final Supplier<MapCodec<LootModifierInjectItem>> CODEC = Suppliers.memoize(() -> RecordCodecBuilder.mapCodec(inst -> codecStart(inst).and(
             inst.group(
-                    ItemStack.CODEC.fieldOf("item").forGetter(LootModifierInjectItem::getItemStack),
+                    ItemStackTemplate.CODEC.fieldOf("item").forGetter(LootModifierInjectItem::getItemStack),
                     Codec.list(Codec.STRING).fieldOf("loot_tables").forGetter(LootModifierInjectItem::getLootTables),
                     Codec.INT.optionalFieldOf("maxStackSize", 1).forGetter(LootModifierInjectItem::getMaxStackSize)
             )
     ).apply(inst, LootModifierInjectItem::new)));
 
-    private final ItemStack itemStack;
+    private final ItemStackTemplate itemStack;
     private final List<String> lootTables;
     private final int maxStackSize;
 
-    public LootModifierInjectItem(LootItemCondition[] conditionsIn, ItemStack itemStack, List<String> lootTables, int maxStackSize) {
+    public LootModifierInjectItem(LootItemCondition[] conditionsIn, ItemStackTemplate itemStack, List<String> lootTables, int maxStackSize) {
         super(conditionsIn);
         this.itemStack = itemStack;
         this.lootTables = lootTables;
         this.maxStackSize = maxStackSize;
     }
 
-    public ItemStack getItemStack() {
+    public ItemStackTemplate getItemStack() {
         return itemStack;
     }
 
@@ -53,7 +54,7 @@ public class LootModifierInjectItem extends LootModifier {
     @Override
     protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
         if (getLootTables().contains(context.getQueriedLootTableId().toString())) {
-            ItemStack stack = getItemStack().copy();
+            ItemStack stack = getItemStack().create();
             stack.setCount(context.getRandom().nextInt(1, getMaxStackSize() + 1));
             generatedLoot.add(stack);
         }
