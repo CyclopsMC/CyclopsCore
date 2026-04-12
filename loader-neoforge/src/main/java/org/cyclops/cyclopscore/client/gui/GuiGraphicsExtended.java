1b05bef84b6f978acf68d71d83464efc5f400879
Ruben Taelman <rubensworks@gmail.com>
2026-04-12 09:11:57 +0200

    Restore removed drawSlotText method, Closes CyclopsMC/IntegratedTerminals#197

--- loader-neoforge/src/main/java/org/cyclops/cyclopscore/client/gui/GuiGraphicsExtended.java
+++ loader-neoforge/src/main/java/org/cyclops/cyclopscore/client/gui/GuiGraphicsExtended.java
@@ -18,14 +18,18 @@ public class GuiGraphicsExtended {
         this.guiGraphics = guiGraphics;
     }
 
+    public void drawSlotText(Font font, String text, int x, int y) {
+        guiGraphics.pose().pushMatrix(); // This part was added
+        float scale = 0.5f; // This part was added
+        guiGraphics.pose().scale(scale, scale); // This part was added
+        guiGraphics.text(font, text, (int) ((x + 19 - 2) / scale - font.width(text)), (int) ((y + 6 + 6) / scale), -1, true); // Scale was added here
+        guiGraphics.pose().popMatrix(); // This part was added
+    }
+
     private void itemCount(Font font, ItemStack itemStack, int x, int y, @org.jspecify.annotations.Nullable String countText) {
         if (itemStack.getCount() != 1 || countText != null) {
             String amount = countText == null ? String.valueOf(itemStack.getCount()) : countText;
-            guiGraphics.pose().pushMatrix(); // This part was added
-            float scale = 0.5f; // This part was added
-            guiGraphics.pose().scale(scale, scale); // This part was added
-            guiGraphics.text(font, amount, (int) ((x + 19 - 2) / scale - font.width(amount)), (int) ((y + 6 + 6) / scale), -1, true); // Scale was added here
-            guiGraphics.pose().popMatrix(); // This part was added
+            drawSlotText(font, amount, x, y);
         }
 
     }
